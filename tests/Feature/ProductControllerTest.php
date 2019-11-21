54c5f166f098434e21a847261662f4b8d9979a27
Duncan McClean <duncan@mcclean.co.uk>
2019-11-21 22:54:27 +0000

    Controller test boilerplate

--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -6,5 +6,39 @@ use Damcclean\Commerce\Tests\TestCase;
 
 class ProductControllerTest extends TestCase
 {
+    /** @test */
+    public function can_view_product_index()
+    {
+        //
+    }
 
+    /** @test */
+    public function can_create_product()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_store_product()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_edit_product()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_update_product()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_destroy_product()
+    {
+        //
+    }
 }
