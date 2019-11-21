c137f5df25f321968e75b13d6c9ac8e55da619d0
Duncan McClean <duncan@mcclean.co.uk>
2019-11-21 23:39:49 +0000

    added more boilerplate tests

---
+++ tests/Feature/CustomerControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Damcclean\Commerce\Tests\Feature;
+
+use Damcclean\Commerce\Tests\TestCase;
+
+class CustomerControllerTest extends TestCase
+{
+    /** @test */
+    public function can_view_customer_index()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_create_customer()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_store_customer()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_edit_customer()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_update_customer()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_destroy_customer()
+    {
+        //
+    }
+}
