d9a81850494c48999a691449cdac59a6c8c23fbe
Duncan McClean <duncan@mcclean.co.uk>
2019-11-23 15:30:55 +0000

    base controllers

---
+++ src/Http/Controllers/CouponController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Damcclean\Commerce\Http\Controllers;
+
+use Statamic\Http\Controllers\CP\CpController;
+
+class CouponController extends CpController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function edit()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
