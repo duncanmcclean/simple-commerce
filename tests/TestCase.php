dc90d2f74d77f75ae50c0384f3bd16014f4e2ebe
Duncan McClean <duncan@mcclean.co.uk>
2019-11-29 19:36:43 +0000

    Fixed the facade error #25

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,19 +2,18 @@
 
 namespace Damcclean\Commerce\Tests;
 
+use Damcclean\Commerce\ServiceProvider;
 use Statamic\Providers\StatamicServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Statamic\Statamic;
 
 abstract class TestCase extends OrchestraTestCase
 {
-    use CreatesApplication;
-
     protected function getPackageProviders($app)
     {
         return [
             StatamicServiceProvider::class,
+            ServiceProvider::class,
         ];
     }
 
--- tests/Unit/BaseModelTest.php
+++ tests/Unit/BaseModelTest.php
@@ -2,13 +2,15 @@
 
 namespace Damcclean\Commerce\Tests\Unit;
 
-use Damcclean\Commerce\Tests\TestCase;
 use Damcclean\Commerce\Models\BaseModel;
+use Damcclean\Commerce\Tests\TestCase as TestCase;
 
 class BaseModelTest extends TestCase
 {
     public function setUp() : void
     {
+        parent::setUp();
+
         $this->base = new BaseModel(__DIR__.'/../stubs/content/commerce/product');
         $this->base->name = 'Product';
         $this->base->slug = 'product';
