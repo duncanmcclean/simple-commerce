726566d02bae955984e1e0e38f9e923ef3b90f5b
Duncan McClean <duncan@mcclean.co.uk>
2019-11-29 19:02:03 +0000

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,21 +4,24 @@ namespace Damcclean\Commerce\Tests;
 
 use Statamic\Providers\StatamicServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Statamic\Statamic;
 
-class TestCase extends OrchestraTestCase
+abstract class TestCase extends OrchestraTestCase
 {
-//    protected function getPackageProviders($app)
-//    {
-//        return [
-//            StatamicServiceProvider::class,
-//        ];
-//    }
-//
-//    protected function getPackageAliases($app)
-//    {
-//        return [
-//            'Statamic' => Statamic::class,
-//        ];
-//    }
+    use CreatesApplication;
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            StatamicServiceProvider::class,
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Statamic' => Statamic::class
+        ];
+    }
 }
