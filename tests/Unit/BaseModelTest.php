2b5399e572b29143c6116fdbb07f9377b98ddbdf
Duncan McClean <duncan@mcclean.co.uk>
2019-12-04 18:03:35 +0000

    wip

--- tests/Unit/BaseModelTest.php
+++ tests/Unit/BaseModelTest.php
@@ -2,19 +2,9 @@
 
 namespace Damcclean\Commerce\Tests\Unit;
 
-use Damcclean\Commerce\Models\BaseModel;
 use Damcclean\Commerce\Tests\TestCase as TestCase;
 
 class BaseModelTest extends TestCase
 {
-//    public function setUp() : void
-//    {
-//        parent::setUp();
-//
-//        $this->base = new BaseModel(__DIR__.'/../stubs/content/commerce/product');
-//        $this->base->name = 'Product';
-//        $this->base->slug = 'product';
-//        $this->base->route = 'products';
-//    }
-
+    //
 }
