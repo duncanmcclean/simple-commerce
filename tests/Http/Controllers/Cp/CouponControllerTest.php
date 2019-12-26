e91dab6afd78da903e624d4f07389acc22fc84cb
Duncan McClean <duncan@mcclean.co.uk>
2019-12-26 19:01:17 +0000

    wip

---
+++ tests/Http/Controllers/Cp/CouponControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Damcclean\Commerce\Tests\Http\Controllers\Cp;
+
+use Damcclean\Commerce\Tests\TestCase;
+
+class CouponControllerTest extends TestCase
+{
+    /** @test */
+    public function can_index_coupons()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_create_coupon()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_store_coupon()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_edit_coupon()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_delete_coupon()
+    {
+        //
+    }
+}
