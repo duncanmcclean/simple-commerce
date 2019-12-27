4e2173195a47ef04ce20df395d7903beea4a4ef6
Duncan McClean <duncan@mcclean.co.uk>
2019-12-27 22:24:59 +0000

    tidy up a wee bit

--- resources/js/components/CommerceListing.vue
+++ resources/js/components/CommerceListing.vue
@@ -23,14 +23,24 @@
                     >
                         <template v-if="primary === 'title'" slot="cell-title" slot-scope="{ row: item }">
                             <div class="flex items-center">
-                                <div v-if="item.enabled != null" class="little-dot mr-1" :class="[item.enabled ? 'bg-green' : 'bg-grey-40']"></div>
+                                <div
+                                        v-if="item.enabled != null"
+                                        class="little-dot mr-1"
+                                        :class="[item.enabled ? 'bg-green' : 'bg-grey-40']"
+                                ></div>
+
                                 <a @click.stop="redirect(item.edit_url)">{{ item.title }}</a>
                             </div>
                         </template>
 
                         <template v-if="primary === 'name'" slot="cell-slug" slot-scope="{ row: item }">
                             <div class="flex items-center">
-                                <div v-if="item.enabled != null" class="little-dot mr-1" :class="[item.enabled ? 'bg-green' : 'bg-grey-40']"></div>
+                                <div
+                                        v-if="item.enabled != null"
+                                        class="little-dot mr-1"
+                                        :class="[item.enabled ? 'bg-green' : 'bg-grey-40']"
+                                ></div>
+
                                 <a @click.stop="redirect(item.edit_url)">{{ item.name }}</a>
                             </div>
                         </template>
