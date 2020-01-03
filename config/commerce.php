41afdc772071c13c3e0d054e1e5b217f54fa80a3
Duncan McClean <duncan@mcclean.co.uk>
2020-01-03 11:19:02 +0000

    Product category pages

--- config/commerce.php
+++ config/commerce.php
@@ -83,6 +83,7 @@ return [
 
         'thanks' => '/thanks', // Page user is redirected to once order has been processed.
         'redeem_coupon' => '/redeem-coupon', // Endpoint where we check if a coupon provided by the customer is valid
+        'category' => '/{category}', // Index page for a product category
 
     ],
 
