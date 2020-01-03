a20774ff094382c9d6b09061e1a8ba058d93b639
Duncan McClean <duncan@mcclean.co.uk>
2020-01-03 11:03:04 +0000

    Fixed things not being passed in correctly

--- src/Http/Controllers/Web/ProductSearchController.php
+++ src/Http/Controllers/Web/ProductSearchController.php
@@ -21,11 +21,25 @@ class ProductSearchController
         $query = $request->input('query');
 
         if (! $query) {
-            $results = Product::all();
+            $results = Product::all()
+                ->map(function ($product) {
+                    return array_merge($product->toArray(), [
+                        'url' => route('products.show', ['product' => $product['slug']]),
+                        'variants' => $product->variants->toArray(),
+                        'from_price' => $product->variants->sortByDesc('price')->first()->price,
+                    ]);
+                });
         } else {
             $results = Product::all()
                 ->filter(function ($item) use ($query) {
                     return false !== stristr((string) $item['title'], $query);
+                })
+                ->map(function ($product) {
+                    return array_merge($product->toArray(), [
+                        'url' => route('products.show', ['product' => $product['slug']]),
+                        'variants' => $product->variants->toArray(),
+                        'from_price' => $product->variants->sortByDesc('price')->first()->price,
+                    ]);
                 });
         }
 
