4d7d0774e3f859a415a61394e585a0c2d713d162
Duncan McClean <duncan@mcclean.co.uk>
2020-01-09 19:18:00 +0000

    use the uid for links

--- resources/views/widgets/recent-orders.blade.php
+++ resources/views/widgets/recent-orders.blade.php
@@ -5,7 +5,7 @@
         <div class="flex flex-col">
             @foreach($orders as $order)
                 <div class="flex flex-row items-center justify-between py-2">
-                    <a href="{{ cp_route('orders.edit', ['order' => $order['id']]) }}">{{ $order['slug'] }}</a>
+                    <a href="{{ cp_route('orders.edit', ['order' => $order['uid']]) }}">{{ $order['slug'] }}</a>
                     <span>{{ \Carbon\Carbon::parse($order['order_date'])->toFormattedDateString() }}</span>
                 </div>
             @endforeach
