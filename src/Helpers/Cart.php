546b89e000e8be85c8636d32c5c137d473e93877
Duncan McClean <duncan@mcclean.co.uk>
2020-01-30 21:34:29 +0000

    Refactor some cart stuff

--- src/Helpers/Cart.php
+++ src/Helpers/Cart.php
@@ -57,10 +57,6 @@ class Cart
         $item->cart_id = $cart->id;
         $item->save();
 
-//        $cart->total += (new CurrencyHelper())->unparse(Variant::where('uid', $data['variant'])->first()->price) * $data['quantity'];
-//        $cart->save();
-        // TODO: figure out how to remove the currency parsed stuff so we can make a cart total
-
         event(new AddedToCart($cart, $item));
 
         return collect($cart->items);
@@ -68,14 +64,10 @@ class Cart
 
     public function remove(string $cartUid, string $itemUid)
     {
-        $cart = CartModel::where('uid', $cartUid)->first();
-
         $item = CartItem::where('uid', $itemUid)->first();
         $item->delete();
 
-        // TODO: actually remove the amount from the cart total
-
-        return CartItem::where('cart_id', $cart->id)->get();
+        return $this->get($cartUid);
     }
 
     public function clear(string $uid)
