1b838f4ad8e5814a7dae60ff5a05b0a981a24160
Duncan McClean <duncan@mcclean.co.uk>
2020-01-31 21:28:29 +0000

    boilerplate of this class

---
+++ tests/Http/Controllers/Web/CheckoutControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CheckoutControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_get_checkout_view()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_store_checkout()
+    {
+        //
+    }
+}
