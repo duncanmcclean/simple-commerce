6bb6a93511ce170d80c0ab4b05eb759d94e08a7a
Duncan McClean <duncan@mcclean.co.uk>
2020-02-01 11:01:58 +0000

    Added testing methods to stripe gateway

--- src/Gateways/StripeGateway.php
+++ src/Gateways/StripeGateway.php
@@ -31,4 +31,102 @@ class StripeGateway
     {
         return PaymentMethod::retrieve($paymentMethod);
     }
+
+    public function randomPaymentMethod(string $type = 'valid', int $expiryYear = 2026)
+    {
+        return PaymentMethod::create([
+            'type' => 'card',
+            'card' => [
+                'number' => $this->{$type}(),
+                'exp_month' => rand(1, 9),
+                'exp_year' => $expiryYear,
+                'cvc' => mt_rand(100, 999),
+            ],
+        ])['id'];
+    }
+
+    public function validCardNumber()
+    {
+        return collect([
+            '4242424242424242',
+            '4000056655665556',
+            '5555555555554444',
+            '2223003122003222',
+        ])->random();
+    }
+
+    public function americanCardNumber()
+    {
+        return collect([
+            '4000000760000002',
+            '4000001240000000',
+            '4012888888881881',
+            '4000004840008001',
+        ])->random();
+    }
+
+    public function europeanCardNumber()
+    {
+        return collect([
+            '4000000400000008',
+            '4000000560000004',
+            '4000002080000001',
+            '4000007240000007',
+            '4000007520000008',
+            '4000007560000009',
+            '4000008260000000',
+            '4000058260000005',
+        ])->random();
+    }
+
+    public function asiaPacificCardNumber()
+    {
+        return collect([
+            '4000000360000006',
+            '4000003440000004',
+            '3530111333300000',
+            '4000005540000008',
+            '4000001560000002',
+        ])->random();
+    }
+
+    public function oneTimePaymentAuth()
+    {
+        return '4000002500003155';
+    }
+
+    public function oneTimePaymentAuthFailure()
+    {
+        return '4000008260003178';
+    }
+
+    public function everyTimePaymentAuth()
+    {
+        return '4000002760003184';
+    }
+
+    public function require3DSecureWithDeclinedCard()
+    {
+        return '4000008400001629';
+    }
+
+    public function incorrectCvc()
+    {
+        return '4000000000000127';
+    }
+
+    public function cardDeclined()
+    {
+        return '4000000000000002';
+    }
+
+    public function cardExpired()
+    {
+        return '4000000000000069';
+    }
+
+    public function highRisk()
+    {
+        return '4000000000004954';
+    }
 }
