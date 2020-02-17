248fc83363fa1ba554cfb06768cbcba81224f337
Duncan McClean <duncan@mcclean.co.uk>
2020-02-17 10:52:08 +0000

    Get the item totals working in cart and checkout views

--- src/Models/CartItem.php
+++ src/Models/CartItem.php
@@ -2,6 +2,7 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Models;
 
+use DoubleThreeDigital\SimpleCommerce\Helpers\Currency as CurrencyHelper;
 use DoubleThreeDigital\SimpleCommerce\Models\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,6 +12,10 @@ class CartItem extends Model
 
     protected $fillable = [
         'uuid', 'product_id', 'variant_id', 'quantity', 'cart_id',
+    ];
+
+    protected $appends = [
+        'total',
     ];
 
     public function cart()
@@ -26,5 +31,10 @@ class CartItem extends Model
     public function variant()
     {
         return $this->belongsTo(Variant::class);
+    }
+
+    public function getTotalAttribute()
+    {
+        return $this->variant->price * $this->attributes['quantity'];
     }
 }
