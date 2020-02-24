dcd3f340bcf94f69a313d94bb28b6fdce7ec7010
Duncan McClean <duncan@mcclean.co.uk>
2020-02-24 17:31:13 +0000

    Re-do the settings page with CP design changes

--- resources/views/cp/settings/index.blade.php
+++ resources/views/cp/settings/index.blade.php
@@ -8,24 +8,19 @@
         <h1 class="mb">Settings</h1>
     </div>
 
-    <div class="flex flex-wrap -mx-2 mt-3">
-        @foreach ($settings as $setting)
-            <div class="w-full md:w-1/3 px-2 mb-4">
-                <div class="card p-0 content border-t-6 border-blue">
-                    <div class="p-2">
-                        <h2><a href="{{ $setting['url'] }}" class="text-grey-90 hover:text-blue">{{ $setting['title'] }}</a></h2>
+    <div class="card p-0 content">
+        <div class="flex flex-wrap">
+            @foreach ($settings as $setting)
+                <a href="{{ $setting['url'] }}" class="w-full lg:w-1/2 p-3 border-t md:flex items-start hover:bg-grey-10 group {{ $loop->odd ? 'lg:border-r' : '' }}">
+                    <div class="h-8 w-8 mr-2 hidden md:block text-grey-80">
+                        @svg($setting['icon'])
+                    </div>
+                    <div class="flex-1 mb-2 md:mb-0 md:mr-3">
+                        <h3 class="mb-1 text-blue group-hover:text-grey-80">{{ $setting['title'] }} &rarr;</h3>
                         <p>{{ $setting['description'] }}</p>
                     </div>
-                    <div class="flex p-2 border-t items-center">
-                        <a href="{{ $setting['url'] }}" class="font-bold text-blue text-sm hover:text-grey-90">{{ $setting['title'] }} &rarr;</a>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-    </div>
-
-    <div class="content">
-        <h2>Looking for more settings?</h2>
-        <p>Simple Commerce provides a load of other settings but for those you'll need to edit the <code>config/simple-commerce.php</code> file.</p>
+                </a>
+            @endforeach
+        </div>
     </div>
 @endsection
--- src/Http/Controllers/Cp/Settings/SettingsHomeController.php
+++ src/Http/Controllers/Cp/Settings/SettingsHomeController.php
@@ -17,16 +17,25 @@ class SettingsHomeController extends CpController
                     'title' => 'Order Statuses',
                     'description' => 'Order statuses help you to organise the status of orders in your store.',
                     'url' => cp_route('settings.order-statuses.index'),
+                    'icon' => 'select',
                 ],
                 [
                     'title' => 'Shipping Zones',
                     'description' => 'Shipping Zones gives you the ability to charge customers a fixed price for shipping to them.',
                     'url' => cp_route('settings.shipping-zones.index'),
+                    'icon' => 'pin',
                 ],
                 [
                     'title' => 'Tax Rates',
                     'description' => 'Manage the tax rates that are added to customer\'s orders when checking out.',
                     'url' => cp_route('settings.tax-rates.index'),
+                    'icon' => 'generic',
+                ],
+                [
+                    'title' => 'More Settings',
+                    'description' => 'Simple Commerce lets you configure more settings in the config/simple-commerce.php file.',
+                    'url' => '#',
+                    'icon' => 'settings-horizontal',
                 ],
             ],
         ]);
