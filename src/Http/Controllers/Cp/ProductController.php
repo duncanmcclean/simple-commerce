19693a0021d86877445230631155c87e38af1bf6
Duncan McClean <duncan@mcclean.co.uk>
2020-03-31 21:13:45 +0100

    wip

--- src/Http/Controllers/Cp/ProductController.php
+++ src/Http/Controllers/Cp/ProductController.php
@@ -6,6 +6,7 @@ use DoubleThreeDigital\SimpleCommerce\Http\Requests\ProductRequest;
 use DoubleThreeDigital\SimpleCommerce\Models\Attribute;
 use DoubleThreeDigital\SimpleCommerce\Models\Product;
 use DoubleThreeDigital\SimpleCommerce\Models\Variant;
+use Illuminate\Support\Arr;
 use Statamic\CP\Breadcrumbs;
 use Statamic\Http\Controllers\CP\CpController;
 
@@ -76,16 +77,17 @@ class ProductController extends CpController
                     'description' => $theVariant['description'],
                 ]);
 
-                collect($theVariant['variant_attributes'])
-                    ->each(function ($attribute) use ($variant) {
-                        if ($attribute['key'] === null) {
-                            return;
-                        }
+                collect($theVariant)
+                    ->each(function ($value, $key) use ($variant) {
+                        if (str_contains($key, 'attributes_')) {
+                            $attributeKey = str_replace('attributes_', '', $key);
+                            $attributeValue = $value;
 
-                        $variant->attributes()->create([
-                            'key' => $attribute['key'],
-                            'value' => $attribute['value'],
-                        ]);
+                            $variant->attributes()->create([
+                                'key'   => $attributeKey,
+                                'value' => $attributeValue,
+                            ]);
+                        }
                     });
             });
 
@@ -107,21 +109,17 @@ class ProductController extends CpController
             ->first();
 
         $values = array_merge($product->toArray(), [
-            'category' => $product->product_category_id,
-            'variants' => $product->variants->map(function (Variant $variant, $key) {
-                return array_merge($variant->toArray(), [
-                    '_id' => 'row-'.$key,
-                    'stock_number' => $variant->stock,
-                    'variant_attributes' => collect($variant->attributes)
-                        ->map(function (Attribute $attribute, $key) {
-                            return [
-                                '_id' => 'row-'.$key,
-                                'uuid' => $attribute->uuid,
-                                'key' => $attribute->key,
-                                'value' => $attribute->value,
-                            ];
-                        })
-                        ->toArray(),
+            'category'  => $product->product_category_id,
+            'variants'  => $product->variants->map(function (Variant $originalVariant, $key) {
+                $variant = $originalVariant->toArray();
+
+                $originalVariant->attributes->each(function (Attribute $attribute) use (&$variant) {
+                    $variant["attributes_{$attribute->key}"] = $attribute['value'];
+                });
+
+                return array_merge($variant, [
+                    '_id'           => "row-{$key}",
+                    'stock_number'  => $variant['stock'], // TODO: need to change the blueprint field handle to be the same as the db column name
                 ]);
             }),
             'product_attributes' => collect($product->attributes)
@@ -224,7 +222,7 @@ class ProductController extends CpController
         $this->authorize('delete', $product);
 
         $product->attributes()->delete();
-        $product->variants()->attributes()->delete();
+        //$product->variants()->attributes()->delete(); // TODO: if variants have no attributes, this goes off on one
         $product->variants()->delete();
         $product->delete();
 
