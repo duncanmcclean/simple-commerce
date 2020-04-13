f05dfb7e41e1b5db9e030178ae37b566f355844d
Duncan McClean <duncan@mcclean.co.uk>
2020-04-13 14:26:20 +0100

    Added soft deletes to attributes
    It's on products and stuff already

--- src/Models/Attribute.php
+++ src/Models/Attribute.php
@@ -5,10 +5,11 @@ namespace DoubleThreeDigital\SimpleCommerce\Models;
 use DoubleThreeDigital\SimpleCommerce\Events\AttributeUpdated;
 use DoubleThreeDigital\SimpleCommerce\Models\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Attribute extends Model
 {
-    use HasUuid;
+    use HasUuid, SoftDeletes;
 
     protected $guarded = [];
 
