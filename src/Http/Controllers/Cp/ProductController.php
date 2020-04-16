aa2f2734c6f7f9a0a4c52ca94e239d106a89dd08
Duncan McClean <duncan@mcclean.co.uk>
2020-04-16 22:36:11 +0100

    Fix issue when updating product

--- src/Http/Controllers/Cp/ProductController.php
+++ src/Http/Controllers/Cp/ProductController.php
@@ -155,7 +155,7 @@ class ProductController extends CpController
             'title'                 => $request->title,
             'slug'                  => $request->slug,
             'description'           => $request->description,
-            'product_category_id'   => $request->category,
+            'product_category_id'   => $request->category[0] ?? null,
             'is_enabled'            => $request->is_enabled === 'true' ? true : false,
             'tax_rate_id'           => $request->tax_rate_id[0] ?? null,
             'needs_shipping'        => $request->needs_shipping,
