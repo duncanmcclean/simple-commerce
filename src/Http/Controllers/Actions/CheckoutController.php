c15c5aa512d129c8808df46b235bb5e703f162df
Duncan McClean <duncan@mcclean.co.uk>
2020-04-21 17:21:20 +0100

    wip

--- src/Http/Controllers/Actions/CheckoutController.php
+++ src/Http/Controllers/Actions/CheckoutController.php
@@ -15,41 +15,56 @@ use DoubleThreeDigital\SimpleCommerce\Models\Country;
 use DoubleThreeDigital\SimpleCommerce\Models\Order;
 use DoubleThreeDigital\SimpleCommerce\Models\OrderStatus;
 use DoubleThreeDigital\SimpleCommerce\Models\State;
+use DoubleThreeDigital\SimpleCommerce\Support\Cart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Event;
 use Statamic\Stache\Stache;
 
 class CheckoutController
 {
-    use UsesCart;
-
-    public function store(CheckoutRequest $request)
+    public function store(Request $request)
     {
-        $this->createCart();
+        $order = Order::where('uuid', Session::get(config('simple-commerce.cart_session_key')))->first();
 
         $payment = (new $request->gateway)->completePurchase($request->all());
-        $payment['gateway'] = $request->gateway;
 
-        $customerModel = config('simple-commerce.customers.model');
-        $customerModel = new $customerModel();
+        if ($payment === true) {
+            $order->update([
+                'is_paid' => true,
+            ]);
 
-        // TODO: needs tidy up
-        try {
-            $customer = $customerModel::where('email', $request->email)->first();
-            $email = $customer->email;
-        } catch (\ErrorException $e) {
-            $fields = $customerModel->fields;
-
-            $customer = new $customerModel();
-            collect($request->all())
-                ->reject(function ($value, $key) use ($fields) {
-                    return !in_array($key, $fields);
-                })
-                ->each(function ($value, $key) use ($customer) {
-                    $customer->{$key} = $value;
-                })
-                ->toArray();
-            $customer->save();
+            Event::dispatch(new OrderPaid($order));
         }
+
+        if (Auth::guest()) {
+            $customerModel = config('simple-commerce.customers.model');
+            $customerModel = new $customerModel();
+
+            $customer = $customerModel::where('email', $request->email)->first();
+
+            if ($customer === null) {
+                $customer = new $customerModel();
+                $fields = $customerModel->fields;
+
+                collect($request->all())
+                    ->reject(function ($value, $key) use ($fields) {
+                        return !in_array($key, $fields);
+                    })
+                    ->each(function ($value, $key) use ($customer) {
+                        $customer->{$key} = $value;
+                    })
+                    ->toArray();
+
+                $customer->save();
+            }
+        } else {
+            $customer = Auth::user();
+        }
+
+        $order->update([
+            'customer_id' => $customer->id,
+        ]);
 
         $billing = Address::updateOrCreate(
             [
@@ -76,44 +91,24 @@ class CheckoutController
         } else {
             $shipping = Address::updateOrCreate(
                 [
-                    'customer_id'   => $request->customer_id,
-                    'address1'      => $request->shipping_address_1,
-                    'zip_code'      => $request->shipping_zip_code,
+                    'customer_id' => $request->customer_id,
+                    'address1' => $request->shipping_address_1,
+                    'zip_code' => $request->shipping_zip_code,
                 ],
                 [
-                    'uuid'          => (new Stache())->generateId(),
-                    'name'          => $customer->name,
-                    'address1'      => $request->shipping_address_1,
-                    'address2'      => $request->shipping_address_2,
-                    'address3'      => $request->shipping_address_3,
-                    'city'          => $request->shipping_city,
-                    'zip_code'      => $request->shipping_zip_code,
-                    'country_id'    => Country::where('iso', $request->shipping_country)->first()->id,
-                    'state_id'      => State::where('abbreviation', $request->shipping_state)->first()->id ?? null,
-                    'customer_id'   => $customer->id,
+                    'uuid' => (new Stache())->generateId(),
+                    'name' => $customer->name,
+                    'address1' => $request->shipping_address_1,
+                    'address2' => $request->shipping_address_2,
+                    'address3' => $request->shipping_address_3,
+                    'city' => $request->shipping_city,
+                    'zip_code' => $request->shipping_zip_code,
+                    'country_id' => Country::where('iso', $request->shipping_country)->first()->id,
+                    'state_id' => State::where('abbreviation', $request->shipping_state)->first()->id ?? null,
+                    'customer_id' => $customer->id,
                 ]
             );
         }
-
-        $order = Order::create([
-            'uuid'                  => (new Stache())->generateId(),
-            'billing_address_id'    => $billing->id,
-            'shipping_address_id'   => $shipping->id,
-            'customer_id'           => $customer->id,
-            'order_status_id'       => OrderStatus::where('primary', true)->first()->id,
-            'items'                 => $this->cart()->orderItems($this->cartId),
-            'total'                 => $this->cart()->total($this->cartId),
-            'currency_id'           => Currency::primary()->id,
-            'gateway_data'          => $payment,
-            'is_paid'               => $payment['is_paid'],
-            'is_refunded'           => false,
-        ]);
-
-        if ($payment['is_paid']) {
-            Event::dispatch(new OrderPaid($order));
-        }
-
-        Event::dispatch(new OrderSuccessful($order));
 
         collect($this->cart()->get($this->cartId))
             ->reject(function (CartItem $cartItem) {
@@ -137,7 +132,12 @@ class CheckoutController
                 }
             });
 
-        $this->replaceCart();
+        $order->update([
+            'is_completed' => true,
+        ]);
+
+        Event::dispatch(new OrderSuccessful($order));
+        Session::remove(config('simple-commerce.cart_session_key'));
 
         return $request->redirect ? redirect($request->redirect) : back();
     }
