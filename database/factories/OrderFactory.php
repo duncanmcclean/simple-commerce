03d357b969fc23e2da6569179dc3568fbb376b5b
Duncan McClean <duncan@mcclean.co.uk>
2020-04-22 16:27:16 +0100

    update some more factory stuff

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -15,27 +15,27 @@ $factory->define(Order::class, function (Faker $faker) {
 
     return [
         'uuid'                  => (new Stache())->generateId(),
+        'gateway'               => '\DoubleThreeDigital\SimpleCommerce\Gateways\DummyGateway',
+        'is_paid'               => false,
+        'is_completed'          => false,
+        'total'                 => 00.00,
+        'item_total'            => 00.00,
+        'tax_total'             => 00.00,
+        'shipping_total'        => 00.00,
+        'currency_id'           => function () {
+            return factory(Currency::class)->create()->id;
+        },
+        'order_status_id'       => function () {
+            return factory(OrderStatus::class)->create()->id;
+        },
         'billing_address_id'    => function () {
             return factory(Address::class)->create()->id;
         },
         'shipping_address_id'   => function () {
             return factory(Address::class)->create()->id;
         },
-        'gateway'               => '\DoubleThreeDigital\SimpleCommerce\Gateways\DummyGateway',
         'customer_id'           => function () {
             return factory(User::class)->create()->id;
-        },
-        'order_status_id'       => function () {
-            return factory(OrderStatus::class)->create()->id;
-        },
-        'item_total'            => 00.00,
-        'tax_total'             => 00.00,
-        'shipping_total'        => 00.00,
-        'total'                 => 00.00,
-        'is_paid'               => false,
-        'is_completed'          => false,
-        'currency_id'           => function () {
-            return factory(Currency::class)->create()->id;
         },
     ];
 });
