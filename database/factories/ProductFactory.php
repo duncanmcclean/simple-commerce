f3b22ff8cf4856ad1fc01c6c665a7e960c888507
Duncan McClean <duncan@mcclean.co.uk>
2020-04-22 15:43:46 +0100

    Remove description from product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -13,7 +13,6 @@ $factory->define(Product::class, function (Faker $faker) {
         'uuid'                  => (new Stache())->generateId(),
         'title'                 => $title,
         'slug'                  => str_slug($title),
-        'description'           => $faker->text,
         'product_category_id'   => function () {
             return factory(ProductCategory::class)->create()->id;
         },
