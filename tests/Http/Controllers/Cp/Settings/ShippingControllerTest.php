5fe63a74216656473aec6690c59422bbf44c2f3b
Duncan McClean <duncan@mcclean.co.uk>
2020-04-25 10:48:04 +0100

    Fixed the shipping settings controller test

--- tests/Http/Controllers/Cp/Settings/ShippingControllerTest.php
+++ tests/Http/Controllers/Cp/Settings/ShippingControllerTest.php
@@ -4,14 +4,14 @@ namespace DoubleThreeDigital\SimpleCommerce\Tests\Http\Controllers\Cp\Settings;
 
 use DoubleThreeDigital\SimpleCommerce\Tests\TestCase;
 
-class ShippingZoneControllerTest extends TestCase
+class ShippingControllerTest extends TestCase
 {
     /** @test */
     public function can_get_index()
     {
         $this
             ->actAsSuper()
-            ->get(cp_route('settings.shipping-zones.index'))
+            ->get(cp_route('settings.shipping.index'))
             ->assertOk()
             ->assertSee('Shipping Zones');
     }
