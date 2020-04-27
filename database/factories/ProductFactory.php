63d08d5800c244e4005fcca326e5ed952bdc928d
Duncan McClean <duncan@mcclean.co.uk>
2020-04-27 15:06:01 +0100

    improve from last commit

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,27 +7,30 @@ use DoubleThreeDigital\SimpleCommerce\Models\Product;
 use Statamic\Stache\Stache;
 
 $factory->define(Product::class, function (Faker $faker) {
+    $titles = [
+        'Ammazon Alexa',
+        'Google Home',
+        'Sunglasses',
+        'Regular Glasses',
+        'Notepad',
+        'Phone Case',
+        'Wireless Router',
+        'Powerline Adapter',
+        'Sphero',
+        'Keyboard',
+        'Mouse',
+        'Monitor',
+        'MacBook Air',
+        'MacBook Pro',
+        'iPhone',
+        'iPad',
+    ];
+    $title = $titles[array_rand($titles)];
+
     return [
         'uuid'                  => (new Stache())->generateId(),
-        'title'                 => array_rand([
-            'Ammazon Alexa',
-            'Google Home',
-            'Sunglasses',
-            'Regular Glasses',
-            'Notepad',
-            'Phone Case',
-            'Wireless Router',
-            'Powerline Adapter',
-            'Sphero',
-            'Keyboard',
-            'Mouse',
-            'Monitor',
-            'MacBook Air',
-            'MacBook Pro',
-            'iPhone',
-            'iPad',
-        ]),
-        'slug'                  => $faker->slug,
+        'title'                 => $title,
+        'slug'                  => str_slug($title),
         'is_enabled'            => true,
         'needs_shipping'        => true,
         'product_category_id'   => function () {
