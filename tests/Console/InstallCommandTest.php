cc40de4bfe22d3e4d4057f5e853000aa33b98096
Duncan McClean <duncan@mcclean.co.uk>
2020-05-11 22:41:14 +0100

    install command test

---
+++ tests/Console/InstallCommandTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\Tests\Console;
+
+use DoubleThreeDigital\SimpleCommerce\Console\Commands\InstallCommand;
+use DoubleThreeDigital\SimpleCommerce\Tests\TestCase;
+
+class InstallCommandTest extends TestCase
+{
+    /** @test */
+    public function it_asks_user_for_confirmation_of_database_before_starting()
+    {
+        $this
+            ->artisan('simple-commerce:install')
+            ->expectsConfirmation('Have you already setup a database?');
+    }
+
+    /** @test */
+    public function an_error_is_shown_if_database_has_not_been_setup()
+    {
+        $this
+            ->artisan('simple-commerce:install')
+            ->expectsConfirmation('Have you already setup a database?', 'no')
+            ->expectsOutput('Please setup a database then run this command again.')
+            ->assertExitCode(0);
+    }
+}
