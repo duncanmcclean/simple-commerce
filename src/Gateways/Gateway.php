2bee3ee97b94655254305a40d7cf45a901c9fa9d
Duncan McClean <duncan@mcclean.co.uk>
2020-05-11 10:35:12 +0100

    Update the Gateway interface

--- src/Gateways/Gateway.php
+++ src/Gateways/Gateway.php
@@ -2,15 +2,18 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Gateways;
 
+use DoubleThreeDigital\SimpleCommerce\Models\Transaction;
+use Illuminate\Support\Collection;
+
 interface Gateway
 {
-    public function completePurchase($data);
+    public function completePurchase(array $data, float $total): Collection;
 
     public function rules(): array;
 
-    public function paymentForm();
+    public function paymentForm(): string;
 
-    public function refund(array $gatewayData);
+    public function refund(Transaction $transaction): Collection;
 
     public function name(): string;
 }
