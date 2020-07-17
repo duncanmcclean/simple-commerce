fc76395824d5e2389ffa052de9b4d999584b0500
Duncan McClean <duncan@mcclean.co.uk>
2020-07-17 20:51:36 +0100

    wip

--- src/Http/Controllers/CouponController.php
+++ src/Http/Controllers/CouponController.php
@@ -2,13 +2,23 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Http\Controllers;
 
+use DoubleThreeDigital\SimpleCommerce\Facades\Cart;
 use Illuminate\Http\Request;
 
 class CouponController extends BaseActionController
 {
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'code' => 'required|string',
+        ]);
+
+        $redeem = Cart::find($request->session()->get(config('simple-commerce.cart_key')))
+            ->redeemCoupon($request->coupon);
+
+        dd($redeem);
+
+        // Return something...
     }
 
     public function destroy(Request $request)
