e4ade72c561628e3bd80da0d6311aeedcaf637fa
Duncan McClean <duncan@mcclean.co.uk>
2020-07-18 08:47:27 +0100

    Work on coupon tag

--- src/Tags/CouponTags.php
+++ src/Tags/CouponTags.php
@@ -3,13 +3,32 @@
 namespace DoubleThreeDigital\SimpleCommerce\Tags;
 
 use DoubleThreeDigital\SimpleCommerce\Facades\Cart;
+use DoubleThreeDigital\SimpleCommerce\Facades\Coupon;
 use Illuminate\Support\Facades\Session;
 
 class CouponTags extends SubTag
 {
     use Concerns\FormBuilder;
 
-    public function redeemed()
+    public function index()
+    {
+        $coupon = Cart::find(Session::get(config('simple-commerce.cart_key')))
+            ->entry()
+            ->data()
+            ->get('coupon');    
+
+        // TODO: ideally, here we'd use an augmented array from Statamic but it wasn't working when trying to implement it
+        
+        $coupon = Coupon::find($coupon);
+
+        return array_merge($coupon->data, [
+            'title' => $coupon->entry()->title,
+            'slug' => $coupon->entry()->slug(),
+            'id' => $coupon->id,
+        ]);
+    }
+
+    public function has()
     {
         return Cart::find(Session::get(config('simple-commerce.cart_key')))
             ->entry()
