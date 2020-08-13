9246058f7cbd95419845b43d8b65f237914faf6f
Duncan McClean <duncan@mcclean.co.uk>
2020-08-13 19:42:07 +0100

    Localize money fieldtype

--- resources/lang/en/fieldtypes.php
+++ resources/lang/en/fieldtypes.php
@@ -3,5 +3,10 @@
 return [
     'money' => [
         'title' => 'Money',
+
+        'config_fields' => [
+            'read_only' => 'Should this field be read only?',
+            'store_as_float' => 'Should this field be stored as a float or just a regular number?',
+        ],
     ],
 ];
--- src/Fieldtypes/MoneyFieldtype.php
+++ src/Fieldtypes/MoneyFieldtype.php
@@ -4,25 +4,29 @@ namespace DoubleThreeDigital\SimpleCommerce\Fieldtypes;
 
 use DoubleThreeDigital\SimpleCommerce\Facades\Currency;
 use Statamic\Facades\Site;
+use Statamic\Fields\ConfigFields;
+use Statamic\Fields\Fields;
 use Statamic\Fields\Fieldtype;
 
 class MoneyFieldtype extends Fieldtype
 {
     protected $icon = 'generic';
 
-    // TODO: translate these
-    protected $configFields = [
-        'read_only' => [
-            'type'         => 'toggle',
-            'instructions' => 'Should this field be read only?',
-            'width'        => 50,
-        ],
-        'store_as_float' => [
-            'type'         => 'toggle',
-            'instructions' => 'Should this field be stored as a float or just a regular number?',
-            'width'        => 50,
-        ],
-    ];
+    public function configFieldItems(): array
+    {
+        return [
+            'read_only' => [
+                'type'         => 'toggle',
+                'instructions' => __('simple-commerce::fieldtypes.money.config_fields.read_only'),
+                'width'        => 50,
+            ],
+            'store_as_float' => [
+                'type'         => 'toggle',
+                'instructions' => __('simple-commerce::fieldtypes.money.config_fields.store_as_float'),
+                'width'        => 50,
+            ],
+        ];
+    }
 
     public function preload()
     {
