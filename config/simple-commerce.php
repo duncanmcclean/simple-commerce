06a6dad7a9fb73b85c911c67fb6ed3fad4b77c56
Duncan McClean <duncan@mcclean.co.uk>
2020-08-24 21:11:11 +0100

    add comments to config

--- config/simple-commerce.php
+++ config/simple-commerce.php
@@ -1,6 +1,17 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sites
+    |--------------------------------------------------------------------------
+    |
+    | For each of your Statamic sites, you can setup a new store which allows you
+    | to use different currencies, tax rates and shipping methods.
+    |
+    */
+
     'sites' => [
         'default' => [
             'currency' => 'GBP',
@@ -18,9 +29,31 @@ return [
         ],
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Gateways
+    |--------------------------------------------------------------------------
+    |
+    | You can setup multiple payment gateways for your store with Simple Commerce.
+    | Here's where you can configure the gateways in use.
+    |
+    */
+
     'gateways' => [
         \DoubleThreeDigital\SimpleCommerce\Gateways\DummyGateway::class => [],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Collections & Taxonomies
+    |--------------------------------------------------------------------------
+    |
+    | Simple Commerce uses Statamic's native collections and taxonomies functionality.
+    | It will automatically create collections/taxonomies upon addon installation if
+    | they don't already exist. However, if you'd like to use a different collection
+    | or taxonomy, like one you've already setup, here's the place to change that.
+    |
+    */
 
     'collections' => [
         'products'  => 'products',
@@ -33,5 +66,16 @@ return [
         'product_categories' => 'product_categories',
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cart Key
+    |--------------------------------------------------------------------------
+    |
+    | Under the hood, Simple Commerce sets an entry in the session to store the customers'
+    | current cart ID. If you want to, you can change the key of the session entry.
+    |
+    */
+
     'cart_key' => 'simple-commerce-cart',
+
 ];
