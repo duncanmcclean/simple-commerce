4d7302b00f33c97cf81053ea3882785500e8dc14
Duncan McClean <duncan@mcclean.co.uk>
2020-09-12 16:43:23 +0100

    Added Address DTO

---
+++ src/Data/Address.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\Data;
+
+class Address
+{
+    public $name;
+    public $address;
+    public $city;
+    public $country;
+    public $postal;
+
+    public function __construct(string $name, string $address, string $city, string $country, string $postal)
+    {
+        $this->name = $name;
+        $this->address = $address;
+        $this->city = $city;
+        $this->country = $country;
+        $this->postal = $postal;
+    }
+
+    public function asArray(): array
+    {
+        return [
+            'name'    => $this->name,
+            'address' => $this->address,
+            'city'    => $this->city,
+            'country' => $this->country,
+            'postal'  => $this->postal,
+        ];
+    }
+
+    public function asString(): string
+    {
+        return collect($this->toArray())
+            ->values()
+            ->join(', ');
+    }
+}
