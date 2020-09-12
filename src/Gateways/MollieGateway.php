178ee2f431991442d5ea23d763093a4b75ca754e
Duncan McClean <duncan@mcclean.co.uk>
2020-09-12 15:46:27 +0100

    wip

--- src/Gateways/MollieGateway.php
+++ src/Gateways/MollieGateway.php
@@ -3,10 +3,16 @@
 namespace DoubleThreeDigital\SimpleCommerce\Gateways;
 
 use DoubleThreeDigital\SimpleCommerce\Contracts\Gateway;
+use DoubleThreeDigital\SimpleCommerce\Facades\Currency;
+use Mollie\Api\MollieApiClient;
+use Mollie\Laravel\Facades\Mollie;
 use Statamic\Entries\Entry;
+use Statamic\Facades\Site;
 
 class MollieGateway extends BaseGateway implements Gateway
 {
+    protected $mollie;
+
     public function name(): string
     {
         return 'Mollie';
@@ -14,7 +20,40 @@ class MollieGateway extends BaseGateway implements Gateway
 
     public function prepare(GatewayPrep $data): GatewayResponse
     {
+        $this->setupMollie();
+        $cart = $data->cart();
 
+        // $payment = $this->mollie->payments->create([
+        //     'amount' => [
+        //         'currency' => Currency::get(Site::current())['code'],
+        //         'value' => substr_replace($cart->data['grand_total'], '.', -2, 0),
+        //     ],
+        //     'description' => 'Order: {$cart->title}',
+        //     'redirectUrl' => 'http://simple-v2.test/!/simple-commerce/gateways/mollie/redirect',
+        //     'webhookUrl'  => 'http://simple-v2.test/!/simple-commerce/gateways/mollie/webhook',
+        // ]);
+
+        // dd($payment);
+
+        $payment = Mollie::api()->payments->create([
+            'amount' => [
+                'currency' => Currency::get(Site::current())['code'],
+                'value' => (string) substr_replace($cart->data['grand_total'], '.', -2, 0),
+            ],
+            'description' => 'Order {$cart->title}',
+            'redirectUrl' => '',
+            'webhookUrl' => '',
+            'metadata' => [
+                'order_id' => $cart->id,
+            ],
+        ]);
+
+        $payment = Mollie::api()->payments->get($payment->id);
+
+        dd($payment);
+
+        // redirect customer to Mollie checkout page
+        return redirect($payment->getCheckoutUrl(), 303);
     }
 
     public function purchase(GatewayPurchase $data): GatewayResponse
@@ -37,5 +76,11 @@ class MollieGateway extends BaseGateway implements Gateway
     public function refundCharge(Entry $order): GatewayResponse
     {
 
+    }
+
+    protected function setupMollie()
+    {
+        $this->mollie = new MollieApiClient();
+        $this->mollie->setApiKey($this->config()['key']);
     }
 }
