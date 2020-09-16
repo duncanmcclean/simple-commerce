b7c9e5ef37b436e893add9befc7cfb7ff36d2ee7
Duncan McClean <duncan@mcclean.co.uk>
2020-09-16 19:39:33 +0100

    wip

--- src/Gateways/MollieGateway.php
+++ src/Gateways/MollieGateway.php
@@ -3,9 +3,10 @@
 namespace DoubleThreeDigital\SimpleCommerce\Gateways;
 
 use DoubleThreeDigital\SimpleCommerce\Contracts\Gateway;
+use DoubleThreeDigital\SimpleCommerce\Facades\Cart;
 use DoubleThreeDigital\SimpleCommerce\Facades\Currency;
+use GuzzleHttp\Client;
 use Mollie\Api\MollieApiClient;
-use Mollie\Laravel\Facades\Mollie;
 use Statamic\Entries\Entry;
 use Statamic\Facades\Site;
 
@@ -23,19 +24,7 @@ class MollieGateway extends BaseGateway implements Gateway
         $this->setupMollie();
         $cart = $data->cart();
 
-        // $payment = $this->mollie->payments->create([
-        //     'amount' => [
-        //         'currency' => Currency::get(Site::current())['code'],
-        //         'value' => substr_replace($cart->data['grand_total'], '.', -2, 0),
-        //     ],
-        //     'description' => 'Order: {$cart->title}',
-        //     'redirectUrl' => 'http://simple-v2.test/!/simple-commerce/gateways/mollie/redirect',
-        //     'webhookUrl'  => 'http://simple-v2.test/!/simple-commerce/gateways/mollie/webhook',
-        // ]);
-
-        // dd($payment);
-
-        $payment = Mollie::api()->payments->create([
+        $payment = $this->mollie->payments->create([
             'amount' => [
                 'currency' => Currency::get(Site::current())['code'],
                 'value' => (string) substr_replace($cart->data['grand_total'], '.', -2, 0),
@@ -48,39 +37,86 @@ class MollieGateway extends BaseGateway implements Gateway
             ],
         ]);
 
-        $payment = Mollie::api()->payments->get($payment->id);
-
-        dd($payment);
-
-        // redirect customer to Mollie checkout page
-        return redirect($payment->getCheckoutUrl(), 303);
+        return new GatewayResponse(true, [], $payment->getCheckoutUrl());
     }
 
     public function purchase(GatewayPurchase $data): GatewayResponse
     {
-
+        return new GatewayResponse(false, []);
     }
 
     public function purchaseRules(): array
     {
-        return [
-
-        ];
+        return [];
     }
 
     public function getCharge(Entry $order): GatewayResponse
     {
+        $this->setupMollie();
+        $cart = Cart::find($order->id());
 
+        $payment = $this->mollie->payments->get($cart->data['gateway_data']['id']);
+
+        return new GatewayResponse(true, [
+            'id' => $payment->id,
+            'mode' => $payment->mode,
+            'amount' => $payment->amount,
+            'settlementAmount' => $payment->settlementAmount,
+            'amountRefunded' => $payment->amountRefunded,
+            'amountRemaining' => $payment->amountRemaining,
+            'description' => $payment->description,
+            'method' => $payment->method,
+            'status' => $payment->status,
+            'createdAt' => $payment->createdAt,
+            'paidAt' => $payment->paidAt,
+            'canceledAt' => $payment->canceledAt,
+            'expiresAt' => $payment->expiresAt,
+            'failedAt' => $payment->failedAt,
+            'profileId' => $payment->profileId,
+            'sequenceType' => $payment->sequenceType,
+            'redirectUrl' => $payment->redirectUrl,
+            'webhookUrl' => $payment->webhookUrl,
+            'mandateId' => $payment->mandateId,
+            'subscriptionId' => $payment->subscriptionId,
+            'orderId' => $payment->orderId,
+            'settlementId' => $payment->settlementId,
+            'locale' => $payment->locale,
+            'metadata' => $payment->metadata,
+            'details' => $payment->details,
+            'restrictPaymentMethodsToCountry' => $payment->restrictPaymentMethodsToCountry,
+            '_links' => $payment->_links,
+            '_embedded' => $payment->_embedded,
+            'isCancelable' => $payment->isCancelable,
+            'amountCaptured' => $payment->amountCaptured,
+            'applicationFeeAmount' => $payment->applicationFeeAmount,
+            'authorizedAt' => $payment->authorizedAt,
+            'expiredAt' => $payment->expiredAt,
+            'customerId' => $payment->customerId,
+            'countryCode' => $payment->countryCode,
+        ]);
     }
 
     public function refundCharge(Entry $order): GatewayResponse
     {
+        $this->setupMollie();
+        $cart = Cart::find($order->id());
 
+        $payment = $this->mollie->payments->get($cart->data['gateway_data']['id']);
+
+        $refund = $payment->refund([]);
+
+        // TODO: add data in the response
+        return new GatewayResponse(true, []);
     }
 
     protected function setupMollie()
     {
-        $this->mollie = new MollieApiClient();
+        $client = new Client([
+            \GuzzleHttp\RequestOptions::VERIFY => \Composer\CaBundle\CaBundle::getBundledCaBundlePath(),
+            \GuzzleHttp\RequestOptions::TIMEOUT => 30, // 30 second timeout
+        ]);
+
+        $this->mollie = new MollieApiClient($client);
         $this->mollie->setApiKey($this->config()['key']);
     }
 }
