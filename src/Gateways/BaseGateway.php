3bf975ada180b19a995565489c791b2f1a649d56
Duncan McClean <duncan@mcclean.co.uk>
2020-09-16 19:16:33 +0100

    New $handle property and method, as well as a callback url method

--- src/Gateways/BaseGateway.php
+++ src/Gateways/BaseGateway.php
@@ -5,14 +5,27 @@ namespace DoubleThreeDigital\SimpleCommerce\Gateways;
 class BaseGateway
 {
     protected array $config = [];
+    protected string $handle = '';
 
-    public function __construct(array $config = [])
+    public function __construct(array $config = [], string $handle = '')
     {
         $this->config = $config;
+        $this->handle = $handle;
     }
 
     public function config(): array
     {
         return $this->config;
+    }
+
+    public function handle(): string
+    {
+        return $this->handle;
+    }
+
+    public function callbackUrl()
+    {
+        // TODO: make this a signed url
+        return config('app.url').route('statamic.simple-commerce.gateways.'.$this->handle.'.callback');
     }
 }
