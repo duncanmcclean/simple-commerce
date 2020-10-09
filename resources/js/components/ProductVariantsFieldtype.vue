59144749f5e345bd407a84cbc4b91e7248ab1039
Duncan McClean <duncan@mcclean.co.uk>
2020-10-09 23:59:09 +0100

    wip

--- resources/js/components/ProductVariantsFieldtype.vue
+++ resources/js/components/ProductVariantsFieldtype.vue
@@ -155,7 +155,7 @@ export default {
 
                     return {
                         key: key,
-                        name: variantName,
+                        variant: variantName,
                         ...existingData,
                     }
                 })
