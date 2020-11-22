77a03a84a1859ed8bb175b70b11c812223967dc8
Duncan McClean <duncan@mcclean.co.uk>
2020-11-22 16:15:52 +0000

    Also enforce order fields

--- src/Listeners/EnforceBlueprintFields.php
+++ src/Listeners/EnforceBlueprintFields.php
@@ -3,6 +3,7 @@
 namespace DoubleThreeDigital\SimpleCommerce\Listeners;
 
 use Statamic\Events\EntryBlueprintFound;
+use Statamic\Fields\Blueprint;
 
 class EnforceBlueprintFields
 {
@@ -12,12 +13,15 @@ class EnforceBlueprintFields
             case 'collections.'.config('simple-commerce.collections.products'):
                 return $this->enforceProductFields($event);
 
+            case 'collections.'.config('simple-commerce.collections.orders'):
+                return $this->enforceOrderFields($event);
+
             default:
                 return;
         }
     }
 
-    protected function enforceProductFields($event)
+    protected function enforceProductFields($event): Blueprint
     {
         if (! $event->blueprint->has('product_variants')) {
             $event->blueprint->ensureField('price', [
@@ -25,5 +29,47 @@ class EnforceBlueprintFields
                 'display' => __('Price'),
             ], 'sidebar');
         }
+
+        return $event->blueprint;
+    }
+
+    protected function enforceOrderFields($event): Blueprint
+    {
+        $event->blueprint->ensureField('grand_total', [
+            'type' => 'money',
+            'display' => __('Grand Total'),
+            'read_only' => true,
+            'validate' => 'required',
+        ]);
+
+        $event->blueprint->ensureField('items_total', [
+            'type' => 'money',
+            'display' => __('Items Total'),
+            'read_only' => true,
+            'validate' => 'required',
+        ]);
+
+        $event->blueprint->ensureField('shipping_total', [
+            'type' => 'money',
+            'display' => __('Shipping Total'),
+            'read_only' => true,
+            'validate' => 'required',
+        ]);
+
+        $event->blueprint->ensureField('tax_total', [
+            'type' => 'money',
+            'display' => __('Tax Total'),
+            'read_only' => true,
+            'validate' => 'required',
+        ]);
+
+        $event->blueprint->ensureField('coupon_total', [
+            'type' => 'money',
+            'display' => __('Coupon Total'),
+            'read_only' => true,
+            'validate' => 'required',
+        ]);
+
+        return $event->blueprint;
     }
 }
