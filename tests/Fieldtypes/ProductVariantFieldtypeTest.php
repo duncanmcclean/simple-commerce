50c5d487de9cc5158aa54bd942836fed2d867ecb
Duncan McClean <duncan@mcclean.co.uk>
2021-01-30 14:15:51 +0000

    Finish tests for product variant fieldtype

--- tests/Fieldtypes/ProductVariantFieldtypeTest.php
+++ tests/Fieldtypes/ProductVariantFieldtypeTest.php
@@ -2,6 +2,8 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Tests\Fieldtypes;
 
+use DoubleThreeDigital\SimpleCommerce\Facades\Product;
+use DoubleThreeDigital\SimpleCommerce\Fieldtypes\ProductVariantFieldtype;
 use DoubleThreeDigital\SimpleCommerce\Tests\TestCase;
 
 class ProductVariantFieldtypeTest extends TestCase
@@ -9,42 +11,93 @@ class ProductVariantFieldtypeTest extends TestCase
     /** @test */
     public function can_preload_and_return_api_route()
     {
-        //
+        $preload = (new ProductVariantFieldtype)->preload();
+
+        $this->assertIsArray($preload);
+        $this->assertStringContainsString('simple-commerce/fieldtype-api/product-variant', $preload['api']);
     }
 
     /** @test */
     public function can_preprocess_value_with_new_format()
     {
-        //
+        $preProcess = (new ProductVariantFieldtype)->preProcess([
+            'product' => 'abcdefg',
+            'variant' => 123456789,
+        ]);
+
+        $this->assertSame($preProcess, [
+            'product' => 'abcdefg',
+            'variant' => 123456789,
+        ]);
     }
 
     /** @test */
     public function can_preproccess_value_with_old_format()
     {
-        //
+        $preProcess = (new ProductVariantFieldtype)->preProcess('abcdefg');
+
+        $this->assertSame($preProcess, [
+            'product' => null,
+            'variant' => 'abcdefg',
+        ]);
     }
 
     /** @test */
     public function that_augmentation_throws_exception_if_old_format()
     {
-        //
+        $this->expectException("\Exception");
+
+        $augment = (new ProductVariantFieldtype)->augment('abcdefg');
     }
 
     /** @test */
     public function that_augmentation_returns_null_if_purcaseable_type_is_product()
     {
-        //
+        $product = Product::make()->save();
+
+        $augment = (new ProductVariantFieldtype)->augment([
+            'product' => $product->id,
+            'variant' => 'One',
+        ]);
+
+        $this->assertNull($augment);
     }
 
     /** @test */
     public function that_augmentation_returns_null_if_variant_does_not_exist()
     {
-        //
+        $product = Product::make()->data([
+            'variants' => [
+                'options' => [
+                    ['key' => 'Yellow_Large', 'variant' => 'Yellow, Large'],
+                ],
+            ]])
+            ->save();
+
+        $augment = (new ProductVariantFieldtype)->augment([
+            'product' => $product->id,
+            'variant' => 'Yellow_Small',
+        ]);
+
+        $this->assertNull($augment);
     }
 
     /** @test */
     public function that_augmentation_returns_variant_data()
     {
-        //
+        $this->markTestSkipped();
+
+        $product = Product::make()->data([
+            'variants' => [
+                'options' => [
+                    ['key' => 'Yellow_Large', 'variant' => 'Yellow, Large'],
+                ],
+            ]])
+            ->save();
+
+        $augment = (new ProductVariantFieldtype)->augment([
+            'product' => $product->id,
+            'variant' => 'Yellow_Large',
+        ]);
     }
 }
