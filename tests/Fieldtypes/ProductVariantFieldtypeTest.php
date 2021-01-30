69cb46bed733559e5106629c7e8629d4380719c1
Duncan McClean <duncan@mcclean.co.uk>
2021-01-30 12:14:26 +0000

    initial testing for fieldtype

---
+++ tests/Fieldtypes/ProductVariantFieldtypeTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\Tests\Fieldtypes;
+
+use DoubleThreeDigital\SimpleCommerce\Tests\TestCase;
+
+class ProductVariantFieldtypeTest extends TestCase
+{
+    /** @test */
+    public function can_preload_and_return_api_route()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_preprocess_value_with_new_format()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_preproccess_value_with_old_format()
+    {
+        //
+    }
+
+    /** @test */
+    public function that_augmentation_throws_exception_if_old_format()
+    {
+        //
+    }
+
+    /** @test */
+    public function that_augmentation_returns_null_if_purcaseable_type_is_product()
+    {
+        //
+    }
+
+    /** @test */
+    public function that_augmentation_returns_null_if_variant_does_not_exist()
+    {
+        //
+    }
+
+    /** @test */
+    public function that_augmentation_returns_variant_data()
+    {
+        //
+    }
+}
