27eb94b93a6c4532294b52a08d5f1ec5e3c92ade
Duncan McClean <duncan@mcclean.co.uk>
2021-02-02 18:42:53 +0000

    Update interfaces
    This should help for anyone needing to override the provided data models.

--- src/Contracts/Order.php
+++ src/Contracts/Order.php
@@ -2,8 +2,44 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Contracts;
 
+use Statamic\Http\Resources\API\EntryResource;
+
 interface Order
 {
+    public function all();
+
+    public function query();
+
+    public function find(string $id): self;
+
+    public function create(array $data = [], string $site = ''): self;
+
+    public function save(): self;
+
+    public function delete();
+
+    public function toResource(): EntryResource;
+
+    public function id();
+
+    public function title(string $title = '');
+
+    public function slug(string $slug = '');
+
+    public function site($site = null): self;
+
+    public function fresh(): self;
+
+    public function data(array $data = []);
+
+    public function has(string $key): bool;
+
+    public function get(string $key);
+
+    public function set(string $key);
+
+    public function toArray(): array;
+
     public function billingAddress();
 
     public function shippingAddress();
@@ -12,5 +48,13 @@ interface Order
 
     public function coupon(string $coupon = '');
 
+    public function redeemCoupon(string $code): bool;
+
     public function markAsCompleted(): self;
+
+    public function boolReceipt(): string;
+
+    public function calculateTotals(): self;
+
+    public static function bindings(): array;
 }
