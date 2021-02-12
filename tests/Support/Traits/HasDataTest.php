18e52f2bc4b257b2cf2a5c4aa78838ff2472407d
Duncan McClean <duncan@mcclean.co.uk>
2021-02-12 20:53:04 +0000

    Use a trait access class for testing

--- tests/Support/Traits/HasDataTest.php
+++ tests/Support/Traits/HasDataTest.php
@@ -7,19 +7,22 @@ use DoubleThreeDigital\SimpleCommerce\Tests\TestCase;
 
 class HasDataTest extends TestCase
 {
-    use HasData;
+    protected TraitAccess $trait;
 
-    public $data;
+    public function setUp(): void
+    {
+        $this->trait = new TraitAccess;
+    }
 
     /** @test */
     public function can_get_all_data()
     {
-        $this->data = [
+        $this->trait->data = [
             'foo' => 'bar',
             'fiz' => 'baa',
         ];
 
-        $data = $this->data();
+        $data = $this->trait->data();
 
         $this->assertIsArray($data);
 
@@ -30,50 +33,50 @@ class HasDataTest extends TestCase
     /** @test */
     public function can_add_to_array()
     {
-        $this->data = [
+        $this->trait->data = [
             'foo' => 'bar',
             'fiz' => 'baa',
         ];
 
-        $data = $this->data([
+        $data = $this->trait->data([
             'joo' => 'mla',
             'dru' => 'pal',
         ]);
 
         $this->assertIsObject($data);
 
-        $this->assertArrayHasKey('foo', $this->data);
-        $this->assertArrayHasKey('fiz', $this->data);
-        $this->assertArrayHasKey('joo', $this->data);
-        $this->assertArrayHasKey('dru', $this->data);
+        $this->assertArrayHasKey('foo', $this->trait->data);
+        $this->assertArrayHasKey('fiz', $this->trait->data);
+        $this->assertArrayHasKey('joo', $this->trait->data);
+        $this->assertArrayHasKey('dru', $this->trait->data);
     }
 
     /** @test */
     public function can_ensure_new_value_updates_existing_value()
     {
-        $this->data = [
+        $this->trait->data = [
             'foo' => 'bar',
             'fiz' => 'baa',
         ];
 
-        $data = $this->data(['foo' => 'barz']);
+        $data = $this->trait->data(['foo' => 'barz']);
 
         $this->assertIsObject($data);
 
-        $this->assertArrayHasKey('foo', $this->data);
-        $this->assertArrayHasKey('fiz', $this->data);
+        $this->assertArrayHasKey('foo', $this->trait->data);
+        $this->assertArrayHasKey('fiz', $this->trait->data);
 
-        $this->assertSame($this->data['foo'], 'barz');
+        $this->assertSame($this->trait->data['foo'], 'barz');
     }
 
     /** @test */
     public function returns_true_if_has_data()
     {
-        $this->data = [
+        $this->trait->data = [
             'foo' => 'bar',
         ];
 
-        $has = $this->has('foo');
+        $has = $this->trait->has('foo');
 
         $this->assertTrue($has);
     }
@@ -81,11 +84,11 @@ class HasDataTest extends TestCase
     /** @test */
     public function returns_false_if_does_not_have_data()
     {
-        $this->data = [
+        $this->trait->data = [
             'foo' => 'bar',
         ];
 
-        $has = $this->has('bar');
+        $has = $this->trait->has('bar');
 
         $this->assertFalse($has);
     }
@@ -93,11 +96,11 @@ class HasDataTest extends TestCase
     /** @test */
     public function can_get_data()
     {
-        $this->data = [
+        $this->trait->data = [
             'foo' => 'bar',
         ];
 
-        $get = $this->get('foo');
+        $get = $this->trait->get('foo');
 
         $this->assertIsString($get);
         $this->assertSame($get, 'bar');
@@ -106,11 +109,11 @@ class HasDataTest extends TestCase
     /** @test */
     public function returns_null_if_data_does_not_exist()
     {
-        $this->data = [
+        $this->trait->data = [
             'foo' => 'bar',
         ];
 
-        $get = $this->get('bar');
+        $get = $this->trait->get('bar');
 
         $this->assertNull($get);
     }
@@ -118,37 +121,37 @@ class HasDataTest extends TestCase
     /** @test */
     public function can_set_new_data()
     {
-        $this->data = [
+        $this->trait->data = [
             'foo' => 'bar',
         ];
 
-        $set = $this->set('bar', 'foo');
+        $set = $this->trait->set('bar', 'foo');
 
-        $this->assertArrayHasKey('bar', $this->data);
-        $this->assertSame($this->data['bar'], 'foo');
+        $this->assertArrayHasKey('bar', $this->trait->data);
+        $this->assertSame($this->trait->data['bar'], 'foo');
     }
 
     /** @test */
     public function can_set_existing_data()
     {
-        $this->data = [
+        $this->trait->data = [
             'foo' => 'bar',
         ];
 
-        $set = $this->set('foo', 'barz');
+        $set = $this->trait->set('foo', 'barz');
 
-        $this->assertArrayHasKey('foo', $this->data);
-        $this->assertSame($this->data['foo'], 'barz');
+        $this->assertArrayHasKey('foo', $this->trait->data);
+        $this->assertSame($this->trait->data['foo'], 'barz');
     }
 
     /** @test */
     public function can_get_data_as_array()
     {
-        $this->data = [
+        $this->trait->data = [
             'foo' => 'bar',
         ];
 
-        $toArray = $this->toArray();
+        $toArray = $this->trait->toArray();
 
         $this->assertIsArray($toArray);
 
@@ -156,8 +159,16 @@ class HasDataTest extends TestCase
             'foo' => 'bar',
         ]);
     }
+}
 
-    // Let's fake the `entry` method, it's called in the `set` method
+// We're using this `TraitAccess` class instead of simply 'using' the class
+// as some of the trait's method name's conflict with those of Testbench's Test Case.
+class TraitAccess
+{
+    public $data;
+
+    use HasData;
+
     protected function entry()
     {
         return new Entry;
