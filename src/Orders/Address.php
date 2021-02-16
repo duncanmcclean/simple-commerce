0a5ea96051954b392da8b63c47b0a87134883a90
Duncan McClean <duncan@mcclean.co.uk>
2021-02-16 21:35:21 +0000

    Add some helper methods

--- src/Orders/Address.php
+++ src/Orders/Address.php
@@ -4,6 +4,8 @@ namespace DoubleThreeDigital\SimpleCommerce\Orders;
 
 class Address
 {
+    // TODO: make these protected in the future?
+    // TODO: also maybe use types on these properties?
     public $name;
     public $address;
     public $city;
@@ -23,11 +25,12 @@ class Address
     public function toArray(): array
     {
         return [
-            'name'     => $this->name,
-            'address'  => $this->address,
-            'city'     => $this->city,
-            'country'  => $this->country,
-            'zip_code' => $this->zipCode,
+            'name'          => $this->name,
+            'address'       => $this->address,
+            'city'          => $this->city,
+            'country'       => $this->country,
+            'zip_code'      => $this->zipCode,
+            'shipping_note' => $this->shippingNote,
         ];
     }
 
@@ -38,7 +41,32 @@ class Address
             ->join(', ');
     }
 
-    public function shippingNote()
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function address(): string
+    {
+        return $this->address;
+    }
+
+    public function city(): string
+    {
+        return $this->city;
+    }
+
+    public function country(): string
+    {
+        return $this->country;
+    }
+
+    public function zipCode(): string
+    {
+        return $this->zipCode;
+    }
+
+    public function shippingNote(): string
     {
         return $this->shippingNote;
     }
