6c619e79105ff2f0035bc4b0931302e5478bb81e
Duncan McClean <duncan@mcclean.co.uk>
2021-02-19 22:53:15 +0000

    Added some blank tests to CalculatorTest

--- tests/Orders/CalculatorTest.php
+++ tests/Orders/CalculatorTest.php
@@ -9,7 +9,62 @@ use DoubleThreeDigital\SimpleCommerce\Tests\TestCase;
 
 class CalculatorTest extends TestCase
 {
-    // TODO: calculator does not run if order is paid
+    /** @test */
+    public function does_not_calculate_totals_if_order_is_paid()
+    {
+        $product = Product::create([
+            'price' => 500,
+        ]);
+
+        $cart = Order::create([
+            'is_paid' => true,
+            'grand_total' => 123,
+            'items_total' => 123,
+            'items' => [
+                [
+                    'product' => $product->id,
+                    'quantity' => 2,
+                    'total' => 123,
+                ],
+            ],
+        ]);
+
+        $calculate = (new Calculator)->calculate($cart);
+
+        $this->assertIsArray($calculate);
+
+        $this->assertSame($calculate['grand_total'], 123);
+        $this->assertSame($calculate['items_total'], 123);
+        $this->assertSame($calculate['shipping_total'], 0);
+        $this->assertSame($calculate['tax_total'], 0);
+        $this->assertSame($calculate['coupon_total'], 0);
+
+        $this->assertSame($calculate['items'][0]['total'], 123);
+    }
+
+    /** @test */
+    public function standard_product_price_is_calculated_correctly()
+    {
+        //
+    }
+
+    /** @test */
+    public function variant_product_price_is_calculated_correctly()
+    {
+        //
+    }
+
+    /** @test */
+    public function ensure_decimals_in_standard_product_prices_are_stripped_out()
+    {
+        //
+    }
+
+    /** @test */
+    public function ensure_decimals_in_variant_product_prices_are_stripped_out()
+    {
+        //
+    }
 
     /** @test */
     public function can_calculate_correct_tax_amount()
@@ -40,5 +95,41 @@ class CalculatorTest extends TestCase
         $this->assertSame($calculate['coupon_total'], 0);
 
         $this->assertSame($calculate['items'][0]['total'], 2000);
+    }
+
+    /** @test */
+    public function ensure_tax_is_subracted_from_item_total_if_included_in_price()
+    {
+        //
+    }
+
+    /** @test */
+    public function ensure_tax_is_not_subtracted_from_item_total_if_not_included_in_prices()
+    {
+        //
+    }
+
+    /** @test */
+    public function ensure_shipping_price_is_applied_correctly()
+    {
+        //
+    }
+
+    /** @test */
+    public function ensure_grand_total_is_calculated_correctly()
+    {
+        //
+    }
+
+    /** @test */
+    public function ensure_percentage_coupon_is_calculated_correctly_on_items_total()
+    {
+        //
+    }
+
+    /** @test */
+    public function ensure_fixed_coupon_is_calculated_correctly_on_items_total()
+    {
+        //
     }
 }
