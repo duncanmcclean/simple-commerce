9a60eb81d33fe37bf76c24fb25be7f80f98d7ed8
Duncan McClean <duncan@mcclean.co.uk>
2021-03-01 16:55:27 +0000

    Provide Ignition solution for no gateway provided

--- src/Exceptions/NoGatewayProvided.php
+++ src/Exceptions/NoGatewayProvided.php
@@ -3,8 +3,18 @@
 namespace DoubleThreeDigital\SimpleCommerce\Exceptions;
 
 use Exception;
+use Facade\IgnitionContracts\BaseSolution;
+use Facade\IgnitionContracts\ProvidesSolution;
+use Facade\IgnitionContracts\Solution;
 
-class NoGatewayProvided extends Exception
+class NoGatewayProvided extends Exception implements ProvidesSolution
 {
-    //
+    public function getSolution(): Solution
+    {
+        return BaseSolution::create("You may have forgotten to provide the Gateway in the request.")
+            ->setSolutionDescription("Ensure that `gateway` is provided in your request. It should be the class name of the gateway you wish to use")
+            ->setDocumentationLinks([
+                'Checkout Tag' => 'https://sc-docs.doublethree.digital/v2.2/tags/checkout-tag',
+            ]);
+    }
 }
