10b510b5cee593c30288d4dccf02d15099a8568d
Duncan McClean <duncan@mcclean.co.uk>
2021-03-09 22:12:33 +0000

    finish calculator tests

--- tests/Orders/CalculatorTest.php
+++ tests/Orders/CalculatorTest.php
@@ -241,13 +241,69 @@ class CalculatorTest extends TestCase
     /** @test */
     public function ensure_tax_is_subracted_from_item_total_if_included_in_price()
     {
-        //
+        Config::set('simple-commerce.sites.default.tax.rate', 20);
+        Config::set('simple-commerce.sites.default.tax.included_in_prices', true);
+
+        $product = Product::create([
+            'price' => 1000,
+        ]);
+
+        $cart = Order::create([
+            'is_paid' => false,
+            'items'   => [
+                [
+                    'product'  => $product->id,
+                    'quantity' => 2,
+                    'total'    => 2000,
+                ],
+            ],
+        ]);
+
+        $calculate = (new Calculator())->calculate($cart);
+
+        $this->assertIsArray($calculate);
+
+        $this->assertSame($calculate['grand_total'], 2000);
+        $this->assertSame($calculate['items_total'], 1667);
+        $this->assertSame($calculate['shipping_total'], 0);
+        $this->assertSame($calculate['tax_total'], 333);
+        $this->assertSame($calculate['coupon_total'], 0);
+
+        $this->assertSame($calculate['items'][0]['total'], 1667);
     }
 
     /** @test */
     public function ensure_tax_is_not_subtracted_from_item_total_if_not_included_in_prices()
     {
-        //
+        Config::set('simple-commerce.sites.default.tax.rate', 20);
+        Config::set('simple-commerce.sites.default.tax.included_in_prices', false);
+
+        $product = Product::create([
+            'price' => 1000,
+        ]);
+
+        $cart = Order::create([
+            'is_paid' => false,
+            'items'   => [
+                [
+                    'product'  => $product->id,
+                    'quantity' => 2,
+                    'total'    => 2000,
+                ],
+            ],
+        ]);
+
+        $calculate = (new Calculator())->calculate($cart);
+
+        $this->assertIsArray($calculate);
+
+        $this->assertSame($calculate['grand_total'], 2333);
+        $this->assertSame($calculate['items_total'], 2000);
+        $this->assertSame($calculate['shipping_total'], 0);
+        $this->assertSame($calculate['tax_total'], 333);
+        $this->assertSame($calculate['coupon_total'], 0);
+
+        $this->assertSame($calculate['items'][0]['total'], 2000);
     }
 
     /** @test */
