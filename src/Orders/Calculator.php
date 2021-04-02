8c10f6ae6f5d5d84f276ce44db7d2248126b5830
Duncan McClean <duncan@mcclean.co.uk>
2021-04-02 11:27:53 +0100

    Apply coupon to grand total of cart

--- src/Orders/Calculator.php
+++ src/Orders/Calculator.php
@@ -172,14 +172,14 @@ class Calculator implements Contract
 
             // Otherwise do all the other stuff...
             if ($coupon->data['type'] === 'percentage') {
-                $data['coupon_total'] = (int) (($value * $data['items_total']) / 100);
+                $data['coupon_total'] = (int) (($value * $data['grand_total']) / 100);
             }
 
             if ($coupon->data['type'] === 'fixed') {
-                $data['coupon_total'] = (int) ($data['items_total'] - $value);
+                $data['coupon_total'] = (int) ($data['grand_total'] - $value);
             }
 
-            $data['items_total'] = (int) str_replace('.', '', (string) ($data['items_total'] - $data['coupon_total']));
+            $data['grand_total'] = (int) str_replace('.', '', (string) ($data['grand_total'] - $data['coupon_total']));
         }
 
         return [
