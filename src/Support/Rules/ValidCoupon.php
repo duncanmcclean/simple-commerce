46e65ec33c632a26d2ad303c115bd449bdd2f587
Duncan McClean <duncan@mcclean.co.uk>
2021-06-03 13:17:28 +0100

    ValidCoupon validation rule

---
+++ src/Support/Rules/ValidCoupon.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\Support\Rules;
+
+use DoubleThreeDigital\SimpleCommerce\Contracts\Order;
+use DoubleThreeDigital\SimpleCommerce\Exceptions\CouponNotFound;
+use DoubleThreeDigital\SimpleCommerce\Facades\Coupon;
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidCoupon implements Rule
+{
+    public $order;
+
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    public function passes($attribute, $value)
+    {
+        try {
+            return Coupon::findByCode($value)->isValid($this->order);
+        } catch (CouponNotFound $e) {
+            return false;
+        }
+    }
+
+    public function message()
+    {
+        return __('simple-commerce::messages.validation.valid_coupon');
+    }
+}
