a8d34fbba1a2d8f0853d1b5af56b98827177dbde
Duncan McClean <duncan@mcclean.co.uk>
2021-06-24 21:33:22 +0100

    Refactor notification listener

--- src/Listeners/SendConfiguredNotifications.php
+++ src/Listeners/SendConfiguredNotifications.php
@@ -2,38 +2,69 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Listeners;
 
-use DoubleThreeDigital\SimpleCommerce\Customers\Customer;
 use DoubleThreeDigital\SimpleCommerce\Events\OrderPaid;
-use DoubleThreeDigital\SimpleCommerce\Notifications\CustomerOrderPaid;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Str;
 
 class SendConfiguredNotifications implements ShouldQueue
 {
     public function handle(OrderPaid $event)
     {
-        foreach (config('simple-commerce.notifications.order_paid') as $notification => $notificationConfig) {
-            if ($notificationConfig['to'] === 'customer') {
-                if ($customer = $event->order->customer()) {
-                    Notification::route('mail', $customer->email())
-                        ->notify(new CustomerOrderPaid($event->order));
-                } elseif ($event->order->has('email')) {
-                    (new Customer)
-                        ->set('email', $this->order->get('customer'))
-                        ->notify(new $notification($event->order));
+        $eventName = Str::of(get_class($event))
+            ->afterLast('\\')
+            ->snake()
+            ->__toString();
+
+        $notifications = collect(Config::get('simple-commerce.notifications'))->get($eventName);
+
+        foreach ($notifications as $notification => $config) {
+            $freshNotification = null;
+
+            $notifiables = $this->getNotifiables($config, $notification, $event);
+            $notification = new $notification($event->order);
+
+            foreach ($notifiables as $channel => $route) {
+                if (! $freshNotification) {
+                    $freshNotification = Notification::route($channel, $route);
+                } else {
+                    $freshNotification->route($channel, $route);
                 }
-
-                continue;
             }
 
-            if (is_string($notificationConfig['to'])) {
-                Notification::route('mail', $notificationConfig['to'])
-                    ->notify(new $notification($event->order));
-
-                continue;
-            }
-
-            throw new \Exception("You did not specify a recipient for the notification [{$notification}]");
+            optional($freshNotification)->notify($notification);
         }
+    }
+
+    protected function getNotifiables(array $config, $notification, $event): array
+    {
+        if ($config['to'] === 'customer') {
+            if ($customer = $event->order->customer()) {
+                return [
+                    ['channel' => 'mail', 'route' => $customer->email()],
+                ];
+            }
+
+            if ($email = $event->order->get('email')) {
+                return [
+                    ['channel' => 'mail', 'route' => $email],
+                ];
+            }
+
+            if ($email = $event->order->get('customer')) {
+                return [
+                    ['channel' => 'mail', 'route' => $email],
+                ];
+            }
+        }
+
+        if (is_string($config['to'])) {
+            return [
+                ['channel' => 'mail', 'route' => $config['to']]
+            ];
+        }
+
+        throw new \Exception("No notifiable specified for [{$notification}]");
     }
 }
