97b26fff7a14d0a7d049e14441626344bf36806d
Duncan McClean <duncan@mcclean.co.uk>
2021-06-26 12:30:10 +0100

    Add new country fieldtype

---
+++ src/Fieldtypes/CountryFieldtype.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\Fieldtypes;
+
+use DoubleThreeDigital\SimpleCommerce\Support\Countries;
+use Statamic\CP\Column;
+use Statamic\Fieldtypes\Relationship;
+
+class CountryFieldtype extends Relationship
+{
+    protected $canCreate = false;
+
+    public function getIndexItems($request)
+    {
+        return Countries::map(function ($country) {
+            return [
+                'id'   => $country['iso'],
+                'iso'  => $country['iso'],
+                'name' => $country['name'],
+            ];
+        })->values();
+    }
+
+    protected function getColumns()
+    {
+        return [
+            Column::make('name'),
+            Column::make('iso'),
+        ];
+    }
+
+    public function toItemArray($id)
+    {
+        $country = Countries::firstWhere('iso', $id);
+
+        return [
+            'id' => $country['iso'],
+            'title' => $country['name'],
+        ];
+    }
+
+    public function preProcessIndex($data)
+    {
+        if (! $data) {
+            return;
+        }
+
+        return collect($data)->map(function ($item) {
+            $country = Countries::firstWhere('iso', $item);
+
+            return $country['name'];
+        })->join(', ');
+    }
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -24,6 +24,7 @@ class ServiceProvider extends AddonServiceProvider
     ];
 
     protected $fieldtypes = [
+        Fieldtypes\CountryFieldtype::class,
         Fieldtypes\MoneyFieldtype::class,
         Fieldtypes\ProductVariantFieldtype::class,
         Fieldtypes\ProductVariantsFieldtype::class,
