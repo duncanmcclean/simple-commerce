385ca471557eb50cdd7e922ddc645384e7d9918c
Duncan McClean <duncan@steadfastcollective.com>
2021-07-07 15:46:19 +0100

    Augment this

--- src/Tags/CartTags.php
+++ src/Tags/CartTags.php
@@ -195,6 +195,10 @@ class CartTags extends SubTag
             return $cart->{$method};
         }
 
+        if (array_key_exists($method, $cart->toAugmentedArray())) {
+            return $cart->toAugmentedArray()[$method];
+        }
+
         if ($cart->has($method)) {
             return $cart->get($method);
         }
