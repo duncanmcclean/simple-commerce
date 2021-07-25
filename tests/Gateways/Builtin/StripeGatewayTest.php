e830bca74d6515fd3d4f9c78474f705b9be83a82
Duncan McClean <duncan@mcclean.co.uk>
2021-07-25 08:46:32 +0100

    Write some actual real life tests

--- tests/Gateways/Builtin/StripeGatewayTest.php
+++ tests/Gateways/Builtin/StripeGatewayTest.php
@@ -2,14 +2,20 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Tests\Gateways\Builtin;
 
+use DoubleThreeDigital\SimpleCommerce\Facades\Customer;
 use DoubleThreeDigital\SimpleCommerce\Gateways\Prepare;
 use DoubleThreeDigital\SimpleCommerce\Facades\Order;
 use DoubleThreeDigital\SimpleCommerce\Facades\Product;
 use DoubleThreeDigital\SimpleCommerce\Gateways\Builtin\StripeGateway;
+use DoubleThreeDigital\SimpleCommerce\Gateways\Purchase;
 use DoubleThreeDigital\SimpleCommerce\Gateways\Response as GatewayResponse;
 use DoubleThreeDigital\SimpleCommerce\Tests\TestCase;
 use Illuminate\Http\Request;
 use Statamic\Facades\Collection;
+use Stripe\Customer as StripeCustomer;
+use Stripe\PaymentIntent;
+use Stripe\PaymentMethod;
+use Stripe\Stripe;
 
 class StripeGatewayTest extends TestCase
 {
@@ -18,10 +24,6 @@ class StripeGatewayTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-
-        if (! env('STRIPE_SECRET')) {
-            $this->markTestSkipped("No STRIPE_SECRET has been defined, tests has been skipped.");
-        }
 
         $this->gateway = new StripeGateway([
             'secret' => env('STRIPE_SECRET'),
@@ -42,7 +44,9 @@ class StripeGatewayTest extends TestCase
     /** @test */
     public function can_prepare()
     {
-        // $this->markTestSkipped();
+        if (! env('STRIPE_SECRET')) {
+            $this->markTestSkipped("Skipping, no Stripe Secret has been defined for this environment.");
+        }
 
         $product = Product::create(['title' => 'Concert Ticket', 'price' => 5500]);
 
@@ -59,6 +63,7 @@ class StripeGatewayTest extends TestCase
                     ],
                 ],
                 'grand_total' => 5500,
+                'title' => '#0001',
             ])
         ));
 
@@ -68,24 +73,180 @@ class StripeGatewayTest extends TestCase
         $this->assertTrue($prepare->success());
         $this->assertArrayHasKey('intent', $prepare->data());
         $this->assertArrayHasKey('client_secret', $prepare->data());
+
+        $paymentIntent = PaymentIntent::retrieve($prepare->data()['intent']);
+
+        $this->assertSame($paymentIntent->id, $prepare->data()['intent']);
+        $this->assertSame($paymentIntent->amount, $order->get('grand_total'));
+        $this->assertNull($paymentIntent->customer);
+        $this->assertNull($paymentIntent->receipt_email);
     }
 
     /** @test */
-    public function can_prepare_with_customer_email()
+    public function can_prepare_with_customer()
     {
-        //
+        if (! env('STRIPE_SECRET')) {
+            $this->markTestSkipped("Skipping, no Stripe Secret has been defined for this environment.");
+        }
+
+        $product = Product::create(['title' => 'Theatre Ticket', 'price' => 1299]);
+        $customer = Customer::create(['name' => 'George', 'email' => 'george@example.com']);
+
+        $prepare = $this->gateway->prepare(new Prepare(
+            new Request(),
+            $order = Order::create([
+                'items' => [
+                    [
+                        'id' => app('stache')->generateId(),
+                        'product' => $product->id,
+                        'quantity' => 1,
+                        'total' => 1299,
+                        'metadata' => [],
+                    ],
+                ],
+                'grand_total' => 1299,
+                'title' => '#0002',
+                'customer' => $customer->id(),
+            ])
+        ));
+
+        $this->assertIsObject($prepare);
+        $this->assertTrue($prepare instanceof GatewayResponse);
+
+        $this->assertTrue($prepare->success());
+        $this->assertArrayHasKey('intent', $prepare->data());
+        $this->assertArrayHasKey('client_secret', $prepare->data());
+
+        $paymentIntent = PaymentIntent::retrieve($prepare->data()['intent']);
+
+        $this->assertSame($paymentIntent->id, $prepare->data()['intent']);
+        $this->assertSame($paymentIntent->amount, $order->get('grand_total'));
+        $this->assertNotNull($paymentIntent->customer);
+        $this->assertNull($paymentIntent->receipt_email);
+
+        $stripeCustomer = StripeCustomer::retrieve($paymentIntent->customer);
+
+        $this->assertSame($stripeCustomer->id, $paymentIntent->customer);
+        $this->assertSame($stripeCustomer->name, 'George');
+        $this->assertSame($stripeCustomer->email, 'george@example.com');
     }
 
     /** @test */
     public function can_prepare_with_receipt_email()
     {
-        //
+        if (! env('STRIPE_SECRET')) {
+            $this->markTestSkipped("Skipping, no Stripe Secret has been defined for this environment.");
+        }
+
+        $product = Product::create(['title' => 'Talent Show Ticket', 'price' => 1299]);
+        $customer = Customer::create(['name' => 'George', 'email' => 'george@example.com']);
+
+        $this->gateway->setConfig([
+            'secret' => env('STRIPE_SECRET'),
+            'receipt_email' => true,
+        ]);
+
+        $prepare = $this->gateway->prepare(new Prepare(
+            new Request(),
+            $order = Order::create([
+                'items' => [
+                    [
+                        'id' => app('stache')->generateId(),
+                        'product' => $product->id,
+                        'quantity' => 1,
+                        'total' => 1299,
+                        'metadata' => [],
+                    ],
+                ],
+                'grand_total' => 1299,
+                'title' => '#0003',
+                'customer' => $customer->id(),
+            ])
+        ));
+
+        $this->assertIsObject($prepare);
+        $this->assertTrue($prepare instanceof GatewayResponse);
+
+        $this->assertTrue($prepare->success());
+        $this->assertArrayHasKey('intent', $prepare->data());
+        $this->assertArrayHasKey('client_secret', $prepare->data());
+
+        $paymentIntent = PaymentIntent::retrieve($prepare->data()['intent']);
+
+        $this->assertSame($paymentIntent->id, $prepare->data()['intent']);
+        $this->assertSame($paymentIntent->amount, $order->get('grand_total'));
+        $this->assertNotNull($paymentIntent->customer);
+        $this->assertSame($paymentIntent->receipt_email, $customer->email());
+
+        $stripeCustomer = StripeCustomer::retrieve($paymentIntent->customer);
+
+        $this->assertSame($stripeCustomer->id, $paymentIntent->customer);
+        $this->assertSame($stripeCustomer->name, 'George');
+        $this->assertSame($stripeCustomer->email, 'george@example.com');
     }
 
     /** @test */
     public function can_purchase()
     {
-        // TODO: Write test for this that doesn't need to touch the Stripe API
+        if (! env('STRIPE_SECRET')) {
+            $this->markTestSkipped("Skipping, no Stripe Secret has been defined for this environment.");
+        }
+
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+
+        $product = Product::create(['title' => 'Zoo Ticket', 'price' => 1234]);
+
+        $order = Order::create([
+            'items' => [
+                [
+                    'id' => app('stache')->generateId(),
+                    'product' => $product->id,
+                    'quantity' => 1,
+                    'total' => 1234,
+                    'metadata' => [],
+                ],
+            ],
+            'grand_total' => 1234,
+            'title' => '#0004',
+            'stripe' => [
+                'intent' => $paymentIntent = PaymentIntent::create([
+                    'amount' => 1234,
+                    'currency' => 'GBP',
+                ])->id,
+            ],
+        ]);
+
+        $paymentMethod = PaymentMethod::create([
+            'type' => 'card',
+            'card' => [
+                'number' => '4242424242424242',
+                'exp_month' => 7,
+                'exp_year' => 2022,
+                'cvc' => '314',
+            ],
+        ]);
+
+        PaymentIntent::retrieve($paymentIntent)->confirm([
+            'payment_method' => $paymentMethod->id,
+        ]);
+
+        $request = new Request(['payment_method' => $paymentMethod->id]);
+
+        $purchase = $this->gateway->purchase(new Purchase($request, $order));
+
+        $this->assertIsObject($purchase);
+        $this->assertTrue($purchase instanceof GatewayResponse);
+
+        $this->assertSame($purchase->data()['id'], $paymentMethod->id);
+        $this->assertSame($purchase->data()['object'], $paymentMethod->object);
+        // $this->assertSame($purchase->data()['card'], $paymentMethod->card);
+        $this->assertSame($purchase->data()['customer'], $paymentMethod->customer);
+        $this->assertSame($purchase->data()['livemode'], $paymentMethod->livemode);
+
+        $order = $order->fresh();
+
+        $this->assertTrue($order->get('is_paid'));
+        $this->assertNotNull($order->get('paud_date'));
     }
 
     /** @test */
