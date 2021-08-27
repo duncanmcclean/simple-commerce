8116e332b2ae3b700751e51aa21d8279c20d07ab
Duncan McClean <duncan@mcclean.co.uk>
2021-08-27 07:17:26 +0100

    Return field meta from product variants fieldtype

--- src/Fieldtypes/ProductVariantsFieldtype.php
+++ src/Fieldtypes/ProductVariantsFieldtype.php
@@ -22,59 +22,68 @@ class ProductVariantsFieldtype extends Fieldtype
 
     public function preload()
     {
-        return [
-            'variant_fields' => [
-                (new Field('name', [
-                    'type'       => 'text',
-                    'listable'   => 'hidden',
-                    'display'    => 'Name',
-                    'width'      => 50,
-                    'input_type' => 'text',
-                    'validate'   => 'required',
-                ]))->toBlueprintArray(),
-                (new Field('values', [
-                    'type'     => 'taggable',
-                    'listable' => 'hidden',
-                    'display'  => 'Values',
-                    'width'    => 50,
-                    'validate' => 'required',
-                ]))->toPublishArray(),
-            ],
-            'option_fields' => array_merge(
-                [
-                    (new Field('key', [
-                        'type'      => 'hidden',
-                        'listable'  => 'hidden',
-                        'display'   => 'Key',
-                        'read_only' => true,
-                        'validate'  => 'required',
-                    ]))->toPublishArray(),
-                    (new Field('variant', [
-                        'type'      => 'textarea',
-                        'listable'  => 'hidden',
-                        'display'   => 'Variant',
-                        'read_only' => true,
-                        'validate'  => 'required',
-                    ]))->toPublishArray(),
-                    (new Field('price', [
-                        'type'      => 'money',
-                        'read_only' => false,
-                        'listable'  => 'hidden',
-                        'display'   => 'price',
-                        'validate'  => 'required',
+        return array_merge(
+            [
+                'variant_fields' => [
+                    (new Field('name', [
+                        'type'       => 'text',
+                        'listable'   => 'hidden',
+                        'display'    => 'Name',
+                        'width'      => 50,
+                        'input_type' => 'text',
+                        'validate'   => 'required',
+                    ]))->toBlueprintArray(),
+                    (new Field('values', [
+                        'type'     => 'taggable',
+                        'listable' => 'hidden',
+                        'display'  => 'Values',
+                        'width'    => 50,
+                        'validate' => 'required',
                     ]))->toPublishArray(),
                 ],
-                collect($this->config('option_fields'))
-                    ->map(function ($field) {
-                        return (
-                            new Field($field['handle'], $field['field'])
-                        )->toPublishArray();
-                    })
-                    ->toArray(),
-            ),
-            'variant' => resolve(Textarea::class)->preload(),
-            'price'   => resolve(MoneyFieldtype::class)->preload(),
-        ];
+                'option_fields' => array_merge(
+                    [
+                        (new Field('key', [
+                            'type'      => 'hidden',
+                            'listable'  => 'hidden',
+                            'display'   => 'Key',
+                            'read_only' => true,
+                            'validate'  => 'required',
+                        ]))->toPublishArray(),
+                        (new Field('variant', [
+                            'type'      => 'textarea',
+                            'listable'  => 'hidden',
+                            'display'   => 'Variant',
+                            'read_only' => true,
+                            'validate'  => 'required',
+                        ]))->toPublishArray(),
+                        (new Field('price', [
+                            'type'      => 'money',
+                            'read_only' => false,
+                            'listable'  => 'hidden',
+                            'display'   => 'price',
+                            'validate'  => 'required',
+                        ]))->toPublishArray(),
+                    ],
+                    collect($this->config('option_fields'))
+                        ->map(function ($field) {
+                            return (
+                                new Field($field['handle'], $field['field'])
+                            )->toPublishArray();
+                        })
+                        ->toArray(),
+                ),
+                'variant' => resolve(Textarea::class)->preload(),
+                'price'   => resolve(MoneyFieldtype::class)->preload(),
+            ],
+            collect($this->config('option_fields'))
+                ->mapWithKeys(function ($field) {
+                    return [$field['handle'] => (
+                        new Field($field['handle'], $field['field'])
+                    )->meta()];
+                })
+                ->toArray(),
+        );
     }
 
     public function preProcess($data)
