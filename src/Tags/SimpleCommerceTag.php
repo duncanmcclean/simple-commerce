4efdfa25976f24f28cfe5a924ee8456b174c237a
Ryan Mitchell <ryan@thoughtcollective.com>
2021-09-29 10:56:51 +0100

    [feature]: add params to sc:countries tags

--- src/Tags/SimpleCommerceTag.php
+++ src/Tags/SimpleCommerceTag.php
@@ -55,7 +55,48 @@ class SimpleCommerceTag extends Tags
 
     public function countries()
     {
-        return Countries::toArray();
+        $countries = collect(Countries::all());
+            
+        if ($inclusions = $this->params->explode('only', [])) {
+
+            $countries = $countries->filter(function ($country) use ($inclusions) {
+                return in_array($country['iso'], $inclusions) OR in_array($country['name'], $inclusions);
+            })->sortBy(function ($country) use ($inclusions) {
+                return array_search($country['iso'], $inclusions);
+            });
+
+        } else {         
+            
+            if ($exclusions = $this->params->explode('exclude', [])) {
+    
+                $countries = $countries->filter(function ($country) use ($exclusions) {
+                    return !(in_array($country['iso'], $exclusions) OR in_array($country['name'], $exclusions));
+                });
+    
+            }
+            
+            if ($common = $this->params->explode('common', [])) {
+                            
+                $common_countries = $countries->filter(function ($country) use ($common) {
+                    return in_array($country['iso'], $common) OR in_array($country['name'], $common);
+                })->sortBy(function ($country) use ($common) {
+                    return array_search($country['iso'], $common);
+                });
+               
+                $common_countries->push([
+                    'iso' => '',
+                    'name' => '-',
+                ]);
+                
+                $countries = $common_countries->concat($countries->filter(function ($country) use ($common) {
+                    return !(in_array($country['iso'], $common) OR in_array($country['name'], $common));
+                }));
+    
+            }
+        
+        }
+            
+        return $countries->toArray();
     }
 
     public function currencies()
