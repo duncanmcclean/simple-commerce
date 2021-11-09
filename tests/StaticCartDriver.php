1f955b4fcc5da2e095112e446b07af7c39d69f9e
Duncan McClean <duncan@steadfastcollective.com>
2021-11-09 21:05:36 +0000

    Add a `StaticCartDriver` for use in tests

---
+++ tests/StaticCartDriver.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\Tests;
+
+use DoubleThreeDigital\SimpleCommerce\Contracts\CartDriver;
+use DoubleThreeDigital\SimpleCommerce\Contracts\Order as OrderContract;
+use DoubleThreeDigital\SimpleCommerce\Facades\Order;
+
+class StaticCartDriver implements CartDriver
+{
+    public static $cart;
+
+    public static function use(): self
+    {
+        app()->bind(CartDriver::class, static::class);
+
+        return new static();
+    }
+
+    public static function setCart(OrderContract $order): self
+    {
+        static::$cart = $order;
+
+        return new static();
+    }
+
+    public function getCartKey(): string
+    {
+        return static::$cart->id();
+    }
+
+    public function getCart(): OrderContract
+    {
+        if ($this->hasCart()) {
+            return static::$cart;
+        }
+
+        return $this->makeCart();
+    }
+
+    public function hasCart(): bool
+    {
+        return is_null(static::$cart);
+    }
+
+    public function makeCart(): OrderContract
+    {
+        static::$cart = Order::create();
+
+        return static::$cart;
+    }
+
+    public function getOrMakeCart(): OrderContract
+    {
+        if ($this->hasCart()) {
+            return $this->getCart();
+        }
+
+        return $this->makeCart();
+    }
+
+    public function forgetCart()
+    {
+        static::$cart = null;
+    }
+}
