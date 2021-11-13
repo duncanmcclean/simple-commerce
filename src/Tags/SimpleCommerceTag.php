551ab02b28659d249355cf40160d57c1fd3272be
Duncan McClean <duncan@mcclean.co.uk>
2021-11-13 08:24:41 +0000

    Fix `{{ sc:errors }}` tag

--- src/Tags/SimpleCommerceTag.php
+++ src/Tags/SimpleCommerceTag.php
@@ -108,7 +108,7 @@ class SimpleCommerceTag extends Tags
 
         $errors = [];
 
-        foreach (session('errors')->getBag('default')->all() as $error) {
+        foreach (session('errors')->getBag('simple-commerce')->all() as $error) {
             $errors[]['value'] = $error;
         }
 
