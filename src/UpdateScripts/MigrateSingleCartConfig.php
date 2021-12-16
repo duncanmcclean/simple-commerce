805f6c487f0451da3ddf3a2d40ab43d409c15605
Duncan McClean <duncan@mcclean.co.uk>
2021-12-16 21:10:55 +0000

    When updating, set a config option to support current behaviour

---
+++ src/UpdateScripts/MigrateSingleCartConfig.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\UpdateScripts;
+
+use Statamic\Facades\Site;
+use Statamic\UpdateScripts\UpdateScript;
+use Stillat\Proteus\Support\Facades\ConfigWriter;
+
+class MigrateSingleCartConfig extends UpdateScript
+{
+    public function shouldUpdate($newVersion, $oldVersion)
+    {
+        return $this->isUpdatingTo('2.4.0-beta.1')
+            && Site::hasMultiple();
+    }
+
+    public function update()
+    {
+        ConfigWriter::write('simple-commerce.cart.single_cart', true);
+
+        $this->console()->info("We've set the 'single_cart' config value to true, for backwards compatibility reasons (due to you running multi-site).");
+    }
+}
