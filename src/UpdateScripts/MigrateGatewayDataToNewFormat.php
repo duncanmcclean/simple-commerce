693b88c82e18f851750288dbe466a71fbd2afe55
Duncan McClean <duncan@mcclean.co.uk>
2021-12-16 16:15:15 +0000

    Migrate existing orders to new format

---
+++ src/UpdateScripts/MigrateGatewayDataToNewFormat.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\UpdateScripts;
+
+use DoubleThreeDigital\SimpleCommerce\Orders\Order;
+use DoubleThreeDigital\SimpleCommerce\SimpleCommerce;
+use Statamic\Contracts\Entries\Entry;
+use Statamic\Facades\Entry as EntryAPI;
+use Statamic\UpdateScripts\UpdateScript;
+
+class MigrateGatewayDataToNewFormat extends UpdateScript
+{
+    public function shouldUpdate($newVersion, $oldVersion)
+    {
+        return $this->isUpdatingTo('2.4.0-beta.1');
+    }
+
+    public function update()
+    {
+        if (SimpleCommerce::orderDriver()['driver'] !== Order::class) {
+            $this->console()->error('Gateway data could not be migrated. You are not using the default Order driver.');
+        }
+
+        EntryAPI::whereCollection(SimpleCommerce::orderDriver()['collection'])
+            ->each(function (Entry $entry) {
+                if (is_string($entry->get('gateway')) && $entry->get('gateway')) {
+                    $gatewayClass = $entry->get('gateway');
+                    $gatewayData = $entry->get('gateway_data') ?? [];
+
+                    $entry
+                        ->set('gateway', [
+                            'use' => $gatewayClass,
+                            'data' => $gatewayData,
+                        ])
+                        ->set('gateway_data', null)
+                        ->save();
+                }
+            });
+
+        $this->console()->info('Migrated gateway data on orders!');
+    }
+}
---
+++ tests/UpdateScripts/MigrateGatewayDataToNewFormatTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\Tests\UpdateScripts;
+
+use DoubleThreeDigital\SimpleCommerce\Gateways\Builtin\DummyGateway;
+use DoubleThreeDigital\SimpleCommerce\Tests\RunsUpdateScripts;
+use DoubleThreeDigital\SimpleCommerce\Tests\SetupCollections;
+use DoubleThreeDigital\SimpleCommerce\Tests\TestCase;
+use DoubleThreeDigital\SimpleCommerce\UpdateScripts\MigrateGatewayDataToNewFormat;
+use DoubleThreeDigital\SimpleCommerce\UpdateScripts\MigrateLineItemMetadata;
+use Illuminate\Support\Facades\File;
+use Statamic\Facades\Entry;
+
+class MigrateGatewayDataToNewFormatTest extends TestCase
+{
+    use RunsUpdateScripts, SetupCollections;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        collect(File::allFiles(base_path('content/collections')))
+            ->each(function ($file) {
+                File::delete($file);
+            });
+    }
+
+    /** @test */
+    public function it_migrates_full_gateway_data_to_new_format()
+    {
+        $this->setupOrders();
+
+        $order = Entry::make()
+            ->collection('orders')
+            ->data([
+                'gateway' => DummyGateway::class,
+                'gateway_data' => [
+                    'foo' => 'bar',
+                    'bar' => 'foo',
+                ],
+            ]);
+
+        $order->save();
+
+        $this->runUpdateScript(MigrateGatewayDataToNewFormat::class);
+
+        $order->fresh();
+
+        $this->assertIsArray($order->get('gateway'));
+        $this->assertNull($order->get('gateway_data'));
+
+        $this->assertSame($order->get('gateway'), [
+            'use' => DummyGateway::class,
+            'data' => [
+                'foo' => 'bar',
+                'bar' => 'foo',
+            ],
+        ]);
+    }
+
+    /** @test */
+    public function it_migrates_gateway_class_to_new_format()
+    {
+        $this->setupOrders();
+
+        $order = Entry::make()
+            ->collection('orders')
+            ->data([
+                'gateway' => DummyGateway::class,
+            ]);
+
+        $order->save();
+
+        $this->runUpdateScript(MigrateGatewayDataToNewFormat::class);
+
+        $order->fresh();
+
+        $this->assertIsArray($order->get('gateway'));
+        $this->assertNull($order->get('gateway_data'));
+
+        $this->assertSame($order->get('gateway'), [
+            'use' => DummyGateway::class,
+            'data' => [],
+        ]);
+    }
+}
