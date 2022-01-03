bcfccd0863ad25ac4360353305f534c29bdf217f
Duncan McClean <duncan@mcclean.co.uk>
2022-01-03 15:01:49 +0000

    Fix currency display issues on widget

--- resources/js/components/Widgets/SalesWidget.vue
+++ resources/js/components/Widgets/SalesWidget.vue
@@ -2,6 +2,7 @@
     <div>
         <div class="flex justify-between items-center mb-2">
             <h3 class="font-bold text-grey">Sales</h3>
+
             <select class="text-xs" v-model="count">
                 <option value="sevenDays">7 Days</option>
                 <option value="fourteenDays">14 Days</option>
@@ -14,9 +15,7 @@
         <div v-if="count === 'thirtyDays'" class="text-4xl mb-2">{{ thirtyDaysCount }}</div>
 
         <div class="flex items-center">
-            <span v-if="count === 'sevenDays'" class="leading-none text-sm">{{ sevenDaysTotal }} total in sales</span>
-            <span v-if="count === 'fourteenDays'" class="leading-none text-sm">{{ fourteenDaysTotal }} total in sales</span>
-            <span v-if="count === 'thirtyDays'" class="leading-none text-sm">{{ thirtyDaysTotal }} total in sales</span>
+            <span class="leading-none text-sm" v-html="totalSalesText"></span>
         </div>
     </div>
 </template>
@@ -38,6 +37,24 @@ export default {
         return {
             count: 'sevenDays',
         }
+    },
+
+    computed: {
+        totalSalesText() {
+            if (this.count === 'sevenDays') {
+                return this.sevenDaysTotal + ' total in sales'
+            }
+
+            if (this.count === 'fourteenDays') {
+                return this.fourteenDaysTotal + ' total in sales'
+            }
+
+            if (this.count === 'thirtyDays') {
+                return this.thirtyDaysTotal + ' total in sales'
+            }
+
+            return 'Unknown'
+        },
     },
 }
 </script>
