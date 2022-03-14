1ef4031d596512f62d181fdc1546fa3579109acf
Duncan McClean <duncan@mcclean.co.uk>
2022-03-14 18:39:36 +0000

    Revert "Add proper validation rules to country & region fieldtypes"
    This reverts commit a21cd1bfa97a46c5f4f781a2526038456b15479c.

--- src/Fieldtypes/CountryFieldtype.php
+++ src/Fieldtypes/CountryFieldtype.php
@@ -57,7 +57,7 @@ class CountryFieldtype extends Relationship
     public function rules(): array
     {
         if ($this->config('max_items') === 1) {
-            return ['string', 'in:' . implode(',', Countries::values()->pluck('iso')->toArray())];
+            return ['string'];
         }
 
         return parent::rules();
