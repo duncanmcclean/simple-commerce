f6c50c993ca1f67fef7ba0067c24e4a9d96c231a
Duncan McClean <duncan@mcclean.co.uk>
2022-04-09 17:56:28 +0100

    Eloquent: Ensure cart cleanup command is compatible with Eloquent

--- src/Console/Commands/CartCleanupCommand.php
+++ src/Console/Commands/CartCleanupCommand.php
@@ -3,6 +3,7 @@
 namespace DoubleThreeDigital\SimpleCommerce\Console\Commands;
 
 use DoubleThreeDigital\SimpleCommerce\Facades\Order;
+use DoubleThreeDigital\SimpleCommerce\SimpleCommerce;
 use Illuminate\Console\Command;
 use Statamic\Console\RunsInPlease;
 
@@ -15,17 +16,41 @@ class CartCleanupCommand extends Command
 
     public function handle()
     {
-        Order::query()
-            ->reject(function ($order) {
-                return $order->isPaid();
-            })
-            ->filter(function ($order) {
-                return $order->date()->isBefore(now()->subDays(14));
-            })
-            ->each(function ($order) {
-                $this->info("Deleting Order {$order->id()}");
+        $this->info('Cleaning up..');
 
-                $order->delete();
-            });
+        if (isset(SimpleCommerce::orderDriver()['collection'])) {
+            Order::query()
+                ->reject(function ($order) {
+                    return $order->isPaid();
+                })
+                ->filter(function ($order) {
+                    return $order->date()->isBefore(now()->subDays(14));
+                })
+                ->each(function ($order) {
+                    $this->line("Deleting order: {$order->id()}");
+
+                    $order->delete();
+                });
+
+            return;
+        }
+
+        if (isset(SimpleCommerce::orderDriver()['model'])) {
+            $orderModelClass = SimpleCommerce::orderDriver()['model'];
+
+            (new $orderModelClass)
+                ->query()
+                ->where('is_paid', false)
+                ->where('created_at', '<', now()->subDays(14))
+                ->each(function ($model) {
+                    $this->line("Deleting order: {$model->id}");
+
+                    $model->delete();
+                });
+
+            return;
+        }
+
+        return $this->error('Unable to cleanup carts with provided cart driver.');
     }
 }
