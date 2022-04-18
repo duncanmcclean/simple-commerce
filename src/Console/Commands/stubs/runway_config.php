fe248203f8175862931bfc9aac15d160ceae13cb
Duncan McClean <duncan@mcclean.co.uk>
2022-04-18 11:02:08 +0100

    Hide Runway resources by default

--- src/Console/Commands/stubs/runway_config.php
+++ src/Console/Commands/stubs/runway_config.php
@@ -15,13 +15,17 @@ return [
         \DoubleThreeDigital\SimpleCommerce\Customers\CustomerModel::class => [
             'name' => 'Customers',
             'handle' => 'customers',
+
             'blueprint' => 'customer',
+            'hidden' => true,
         ],
 
         \DoubleThreeDigital\SimpleCommerce\Orders\OrderModel::class => [
             'name' => 'Orders',
             'handle' => 'orders',
+
             'blueprint' => 'order',
+            'hidden' => true,
             'read_only' => true,
         ],
     ],
