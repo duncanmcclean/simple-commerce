dd15e158be49efea87a12bee1291e0c079c606e3
Duncan McClean <duncan@mcclean.co.uk>
2022-04-19 23:08:26 +0100

    wip

--- resources/views/cp/overview.blade.php
+++ resources/views/cp/overview.blade.php
@@ -1,9 +1,72 @@
 @extends('statamic::layout')
 @section('title', 'Overview')
-@section('wrapper_class', 'max-w-full')
 
 @section('content')
-<div class="flex items-center justify-between mb-3">
-    <h1 class="flex-1">Overview</h1>
+<header class="mb-3">
+    <h1>{{ __('Overview') }}</h1>
+
+    <!-- Toggle widgets (will tie in with user preferences) -->
+</header>
+
+<div class="card p-2 content mb-2">
+    <p>TODO: Orders chart</p>
 </div>
+
+<div class="grid grid-cols-2 gap-2">
+    <div class="flex-1 card p-0 overflow-hidden h-full">
+        <div class="flex justify-between items-center p-2">
+            <h2>
+                <span>{{ __('Recent Orders') }}</span>
+            </h2>
+        </div>
+
+        <ul class="px-2">
+            @foreach($recentOrders as $order)
+                <li class="py-1 flex items-center justify-between">
+                    <a href="#"><strong>#{{ $order->orderNumber() }}</strong> - {{ \DoubleThreeDigital\SimpleCommerce\Currency::parse($order->grandTotal(), \Statamic\Facades\Site::current()) }}</a>
+                    <span class="text-sm">{{ $order->get('paid_date') }}</span>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
+    <div class="flex-1 card p-0 overflow-hidden h-full">
+        <div class="flex justify-between items-center p-2">
+            <h2>
+                <span>{{ __('Top Customers') }}</span>
+            </h2>
+        </div>
+
+        <ul class="px-2">
+            @foreach($topCustomers as $customer)
+                <li class="py-1 flex items-center justify-between">
+                    <a href="#">{{ $customer->email() }}</a>
+                    <span class="text-sm">{{ $customer->orders()->count() }} orders</span>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
+    <div class="flex-1 card p-0 overflow-hidden h-full">
+        <div class="flex justify-between items-center p-2">
+            <h2>
+                <span>{{ __('Low Stock Products') }}</span>
+            </h2>
+        </div>
+
+        <ul class="px-2">
+            @foreach($lowStockProducts as $product)
+                <li class="py-1 flex items-center justify-between">
+                    <a href="#">{{ $product->get('title') }}</a>
+                    <span class="text-sm @if($product->stock() == 0) text-red-light @endif">{{ $product->stock() }} remaining</span>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</div>
+
+@include('statamic::partials.docs-callout', [
+    'topic' => 'Simple Commerce',
+    'url' => 'https://simple-commerce.duncanmcclean.com/?ref=cp_overview'
+])
 @endsection
--- src/Http/Controllers/CP/OverviewController.php
+++ src/Http/Controllers/CP/OverviewController.php
@@ -2,10 +2,91 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Http\Controllers\CP;
 
+use DoubleThreeDigital\SimpleCommerce\Facades\Customer;
+use DoubleThreeDigital\SimpleCommerce\Facades\Order;
+use DoubleThreeDigital\SimpleCommerce\Facades\Product;
+use DoubleThreeDigital\SimpleCommerce\SimpleCommerce;
+use Illuminate\Http\Request;
+use Statamic\Facades\Collection;
+
 class OverviewController
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('simple-commerce::cp.overview');
+        return view('simple-commerce::cp.overview', [
+            'recentOrders' => $this->getRecentOrders($request),
+            'topCustomers' => $this->getTopCustomers($request),
+            'lowStockProducts' => $this->getLowStockProducts($request),
+        ]);
+    }
+
+    protected function getRecentOrders($request)
+    {
+        if (isset(SimpleCommerce::orderDriver()['collection'])) {
+            $query = Collection::find(SimpleCommerce::orderDriver()['collection'])
+                ->queryEntries()
+                ->where('is_paid', true)
+                ->orderBy('paid_date', 'desc')
+                ->limit(5)
+                ->get()
+                ->map(function ($entry) {
+                    return Order::find($entry->id());
+                })
+                ->values();
+
+            return $query;
+        }
+
+        // TODO: implement Eloquent query
+
+        return null;
+    }
+
+    protected function getTopCustomers($request)
+    {
+        if (isset(SimpleCommerce::customerDriver()['collection'])) {
+            $query = Collection::find(SimpleCommerce::customerDriver()['collection'])
+                ->queryEntries()
+                ->get()
+                ->sortByDesc(function ($customer) {
+                    return count($customer->get('orders', []));
+                })
+                ->take(5)
+                ->map(function ($entry) {
+                    return Customer::find($entry->id());
+                })
+                ->values();
+
+            return $query;
+        }
+
+        // TODO: implement Eloquent query
+        // TODO: implement User query
+
+        return null;
+    }
+
+    protected function getLowStockProducts($request)
+    {
+        if (isset(SimpleCommerce::productDriver()['collection'])) {
+            $query = Collection::find(SimpleCommerce::productDriver()['collection'])
+                ->queryEntries()
+                ->where('stock', '<', config('simple-commerce.low_stock_threshold'))
+                ->orderBy('stock', 'asc')
+                ->get()
+                ->reject(function ($entry) {
+                    return $entry->has('product_variants')
+                        || ! $entry->has('stock');
+                })
+                ->take(5)
+                ->map(function ($entry) {
+                    return Product::find($entry->id());
+                })
+                ->values();
+
+            return $query;
+        }
+
+        return null;
     }
 }
