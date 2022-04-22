414d779a0f720fd66202b37e9841075924920f60
Duncan McClean <duncan@mcclean.co.uk>
2022-04-22 23:30:27 +0100

    wip

--- src/Overview.php
+++ src/Overview.php
@@ -17,32 +17,7 @@ class Overview
 
     public static function widgets(): array
     {
-        // dd(static::$widgets);
-
         return static::$widgets;
-
-        return [
-            [
-                'name' => 'Orders Chart',
-                'handle' => 'orders-chart',
-                'component' => 'overview-orders-chart',
-            ],
-            // [
-            //     'name' => 'Recent Orders',
-            //     'handle' => 'recent-orders',
-            //     'component' => 'overview-recent-orders',
-            // ],
-            // [
-            //     'name' => 'Top Customers',
-            //     'handle' => 'top-customers',
-            //     'component' => 'overview-top-customers',
-            // ],
-            // [
-            //     'name' => 'Low Stock Products',
-            //     'handle' => 'low-stock-products',
-            //     'component' => 'overview-low-stock-products',
-            // ],
-        ];
     }
 
     public static function widget(string $handle): ?array
