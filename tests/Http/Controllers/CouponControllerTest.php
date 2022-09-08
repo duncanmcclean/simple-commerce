duncanmcclean/simple-commerce

commit cef5aa02b09d06e051f1153ebce2426b85374524

Implement PHP enums (#712)
* Implement proper enums

* Remove spatie/enum package

src/Coupons/Coupon.php
@@ -47,17 +47,16 @@ class Coupon implements Contract
         return $this
             ->fluentlyGetOrSet('type')
             ->setter(function ($value) {
-                // Cast to enum
                 if ($value === 'fixed') {
-                    $value = CouponType::FIXED();
+                    $value = CouponType::Fixed;
                 }
 
                 if ($value === 'percentage') {
-                    $value = CouponType::PERCENTAGE();
+                    $value = CouponType::Percentage;
                 }
 
                 // If the value is already set, it's over 100 and it's a percentage, we want to divide it by 100...
-                if ($value === CouponType::PERCENTAGE() && $this->value > 100) {
+                if ($value === CouponType::Percentage && $this->value > 100) {
                     $this->value = $this->value / 100;
                 }
 
@@ -75,7 +74,7 @@ class Coupon implements Contract
                     $value = (int) str_replace('.', '', $value);
                 }
 
-                if ($this->type === CouponType::PERCENTAGE() && $value > 100) {
+                if ($this->type === CouponType::Percentage && $value > 100) {
                     return $value / 100;
                 }
 

resources/views/cp/coupons/index.blade.php
@@ -36,7 +36,7 @@
                             {{ $coupon->get('description') ?? '-' }}
                         </td>
                         <td>
-                            @if($coupon->type() === \DoubleThreeDigital\SimpleCommerce\Coupons\CouponType::PERCENTAGE())
+                            @if($coupon->type() === \DoubleThreeDigital\SimpleCommerce\Coupons\CouponType::Percentage)
                                 {{ $coupon->value() }}% off
                             @else
                                 {{ \DoubleThreeDigital\SimpleCommerce\Currency::parse($coupon->value(), \Statamic\Facades\Site::current()) }} off

src/Coupons/CouponType.php
@@ -2,12 +2,8 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Coupons;
 
-use Spatie\Enum\Enum;
-
-/**
- * @method static self FIXED()
- * @method static self PERCENTAGE()
- */
-class CouponType extends Enum
+enum CouponType: string
 {
+    case Fixed = 'fixed';
+    case Percentage = 'percentage';
 }

src/Exceptions/CheckoutProductHasNoStockException.php
@@ -15,11 +15,11 @@ class CheckoutProductHasNoStockException extends \Exception
         $this->product = $product;
         $this->variant = $variant;
 
-        if ($product->purchasableType() === ProductType::PRODUCT()) {
+        if ($product->purchasableType() === ProductType::Product) {
             $message = "Product [{$product->id()}] does not have any available stock.";
         }
 
-        if ($product->purchasableType() === ProductType::VARIANT()) {
+        if ($product->purchasableType() === ProductType::Variant) {
             $message = "Variant [{$variant->key()}] on [{$product->id()}] does not have any available stock.";
         }
 

src/Fieldtypes/ProductVariantFieldtype.php
@@ -44,7 +44,7 @@ class ProductVariantFieldtype extends Fieldtype
 
         $product = Product::find($value['product']);
 
-        if ($product->purchasableType() === ProductType::PRODUCT()) {
+        if ($product->purchasableType() === ProductType::Product) {
             return null;
         }
 

src/Http/Controllers/CartItemController.php
@@ -106,11 +106,11 @@ class CartItemController extends BaseActionController
         }
 
         // Ensure there's enough stock to fulfill the customer's quantity
-        if ($product->purchasableType() === ProductType::PRODUCT()) {
+        if ($product->purchasableType() === ProductType::Product) {
             if ($product->stock() && $product->stock() !== null && $product->stock() < $request->quantity) {
                 return $this->withErrors($request, __("There's not enough stock to fulfil the quantity you selected. Please try again later."));
             }
-        } elseif ($product->purchasableType() === ProductType::VARIANT()) {
+        } elseif ($product->purchasableType() === ProductType::Variant) {
             $variant = $product->variant($request->get('variant'));
 
             if ($variant !== null && $variant->stock() !== null && $variant->stock() < $request->quantity) {

src/Orders/Calculator.php
@@ -79,7 +79,7 @@ class Calculator implements Contract
     {
         $product = ProductAPI::find($lineItem['product']);
 
-        if ($product->purchasableType() === ProductType::VARIANT()) {
+        if ($product->purchasableType() === ProductType::Variant) {
             $variant = $product->variant(
                 isset($lineItem['variant']['variant']) ? $lineItem['variant']['variant'] : $lineItem['variant']
             );
@@ -173,11 +173,11 @@ class Calculator implements Contract
             $baseAmount = $data['items_total'] + $data['tax_total'];
 
             // Otherwise do all the other stuff...
-            if ($coupon->type() === CouponType::PERCENTAGE()) {
+            if ($coupon->type() === CouponType::Percentage) {
                 $data['coupon_total'] = (int) ($value * $baseAmount) / 100;
             }
 
-            if ($coupon->type() === CouponType::FIXED()) {
+            if ($coupon->type() === CouponType::Fixed) {
                 $data['coupon_total'] = (int) $baseAmount - ($baseAmount - $value);
             }
 

src/Orders/Checkout/HandleStock.php
@@ -18,7 +18,7 @@ class HandleStock
             ->each(function (LineItem $item) {
                 $product = $item->product();
 
-                if ($product->purchasableType() === ProductType::PRODUCT()) {
+                if ($product->purchasableType() === ProductType::Product) {
                     if (is_int($product->stock())) {
                         $stock = $product->stock() - $item->quantity();
 
@@ -41,7 +41,7 @@ class HandleStock
                     }
                 }
 
-                if ($product->purchasableType() === ProductType::VARIANT()) {
+                if ($product->purchasableType() === ProductType::Variant) {
                     $variant = $product->variant($item->variant()['variant'] ?? $item->variant());
 
                     if ($variant !== null && $variant->stock() !== null) {

commit 13660d476fb3385e5660843a7ae8d86d0b7cc963

Coupons: Ability to disable coupons (#713)
* Ability to disable coupons

* Fix coupon types not being saved correctly

Caused by #712

* Resolve failing tests

* Add test to confirm disabled coupon can't be redeemed

src/Coupons/Coupon.php
@@ -20,12 +20,14 @@ class Coupon implements Contract
     public $code;
     public $value;
     public $type;
+    public $enabled;
 
     protected $selectedQueryRelations = [];
 
     public function __construct()
     {
         $this->data = collect();
+        $this->enabled = true;
     }
 
     public function id($id = null)
@@ -83,9 +85,20 @@ class Coupon implements Contract
             ->args(func_get_args());
     }
 
+    public function enabled($enabled = null)
+    {
+        return $this
+            ->fluentlyGetOrSet('enabled')
+            ->args(func_get_args());
+    }
+
     public function isValid(Order $order): bool
     {
         $order = OrderFacade::find($order->id());
+
+        if (! $this->enabled()) {
+            return false;
+        }
 
         if ($this->has('minimum_cart_value') && $order->itemsTotal()) {
             if ($order->itemsTotal() < $this->get('minimum_cart_value')) {
@@ -185,6 +198,7 @@ class Coupon implements Contract
         $this->code = $freshCoupon->code;
         $this->value = $freshCoupon->value;
         $this->type = $freshCoupon->type;
+        $this->enabled = $freshCoupon->enabled;
         $this->data = $freshCoupon->data();
 
         return $this;
@@ -193,10 +207,11 @@ class Coupon implements Contract
     public function toArray(): array
     {
         return array_merge($this->data()->toArray(), [
-            'id' => $this->id(),
-            'code' => $this->code(),
-            'value' => $this->value(),
-            'type' => $this->type(),
+            'id' => $this->id,
+            'code' => $this->code,
+            'value' => $this->value,
+            'type' => optional($this->type)->value,
+            'enabled' => $this->enabled,
         ]);
     }
 

src/Coupons/CouponBlueprint.php
@@ -99,6 +99,12 @@ class CouponBlueprint
             'sidebar' => [
                 'display' => 'Sidebar',
                 'fields' => [
+                    'enabled' => [
+                        'display' => 'Enabled?',
+                        'type' => 'toggle',
+                        'instructions' => 'When disabled, this coupon will not be redeemable.',
+                        'default' => true,
+                    ],
                     'redeemed' => [
                         'input_type' => 'number',
                         'type' => 'text',

src/Coupons/CouponStore.php
@@ -28,7 +28,8 @@ class CouponStore extends BasicStore
             ->code(array_pull($data, 'code'))
             ->type(array_pull($data, 'type'))
             ->value(array_pull($data, 'value'))
-            ->data(Arr::except($data, ['code', 'type', 'value']));
+            ->enabled(array_pull($data, 'enabled') ?? true)
+            ->data(Arr::except($data, ['code', 'type', 'value', 'enabled']));
 
         if (isset($idGenerated)) {
             $coupon->save();

src/Http/Controllers/CP/CouponController.php
@@ -42,7 +42,8 @@ class CouponController
             ->code(Str::upper($request->code))
             ->type($request->type)
             ->value($request->value)
-            ->data(Arr::except($request->validated(), ['code', 'type', 'value']));
+            ->enabled($request->enabled ?? true)
+            ->data(Arr::except($request->validated(), ['code', 'type', 'value', 'enabled']));
 
         $coupon->save();
 
@@ -82,7 +83,8 @@ class CouponController
             ->code(Str::upper($request->code))
             ->type($request->type)
             ->value($request->value)
-            ->data(Arr::except($request->validated(), ['code', 'type', 'value']))
+            ->enabled($request->enabled ?? true)
+            ->data(Arr::except($request->validated(), ['code', 'type', 'value', 'enabled']))
             ->save();
 
         return [

src/Http/Requests/CP/Coupon/StoreRequest.php
@@ -61,6 +61,10 @@ class StoreRequest extends FormRequest
                 'nullable',
                 'array',
             ],
+            'enabled' => [
+                'required',
+                'boolean',
+            ],
         ];
     }
 }

src/Http/Requests/CP/Coupon/UpdateRequest.php
@@ -55,6 +55,10 @@ class UpdateRequest extends FormRequest
                 'nullable',
                 'array',
             ],
+            'enabled' => [
+                'required',
+                'boolean',
+            ],
         ];
     }
 }

src/UpdateScripts/v4_0/MigrateCouponsToStache.php
@@ -27,6 +27,7 @@ class MigrateCouponsToStache extends UpdateScript
                     ->code(Str::upper($entry->slug()))
                     ->type($entry->get('type'))
                     ->value($entry->get('coupon_value') ?? $entry->get('value'))
+                    ->enabled($entry->get('enabled') ?? true)
                     ->data(array_merge(
                         $entry->data()->except([
                             'updated_at',

tests/Http/Controllers/CP/CouponControllerTest.php
@@ -51,11 +51,18 @@ class CouponControllerTest extends TestCase
                 'type' => 'percentage',
                 'value' => 30,
                 'description' => '30% discount on a Thursday!',
+                'enabled' => true,
             ])
             ->assertJsonStructure([
                 'redirect',
             ])
             ->assertSessionHasNoErrors();
+
+        $coupon = Coupon::findByCode('thursday-thirty');
+
+        $this->assertSame($coupon->value(), 30);
+        $this->assertSame($coupon->enabled(), true);
+        $this->assertSame($coupon->get('description'), '30% discount on a Thursday!');
     }
 
     /** @test */
@@ -145,6 +152,7 @@ class CouponControllerTest extends TestCase
                 'type' => 'percentage',
                 'value' => 51,
                 'description' => 'You can actually get a 51% discount on Friday!',
+                'enabled' => false,
             ])
             ->assertJsonStructure([
                 'coupon',
@@ -153,6 +161,7 @@ class CouponControllerTest extends TestCase
         $coupon->fresh();
 
         $this->assertSame($coupon->value(), 51);
+        $this->assertSame($coupon->enabled(), false);
         $this->assertSame($coupon->get('description'), 'You can actually get a 51% discount on Friday!');
     }
 

tests/Http/Controllers/CouponControllerTest.php
@@ -358,6 +358,44 @@ class CouponControllerTest extends TestCase
     }
 
     /** @test */
+    public function cant_store_coupon_when_coupon_is_disabled()
+    {
+        $this->buildCartWithProducts();
+
+        $coupon = Coupon::make()
+            ->id(Stache::generateId())
+            ->code('halv-price')
+            ->value(50)
+            ->type('percentage')
+            ->enabled(false)
+            ->data([
+                'description'        => 'Halv Price',
+                'redeemed'           => 0,
+                'minimum_cart_value' => null,
+            ]);
+
+        $coupon->save();
+        $coupon->fresh();
+
+        $data = [
+            'code' => 'halv-price',
+        ];
+
+        $response = $this
+            ->from('/cart')
+            ->withSession(['simple-commerce-cart' => $this->cart->id])
+            ->post(route('statamic.simple-commerce.coupon.store'), $data);
+
+        $response->assertRedirect('/cart');
+        $response->assertSessionHasErrors();
+
+        $this->cart->fresh();
+
+        $this->assertNull($this->cart->coupon());
+        $this->assertSame($this->cart->couponTotal(), 0000);
+    }
+
+    /** @test */
     public function can_destroy_coupon()
     {
         $this->buildCartWithProducts();