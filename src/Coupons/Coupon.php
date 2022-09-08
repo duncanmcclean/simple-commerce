duncanmcclean/simple-commerce

commit cef5aa02b09d06e051f1153ebce2426b85374524

Implement PHP enums (#712)
* Implement proper enums

* Remove spatie/enum package

src/Coupons/Coupon.php
@@ -47,17 +47,16 @@ class Coupon implements Contract
         return $this
             ->fluentlyGetOrSet('type')
             ->setter(function ($value) {
-                // Cast to enum
                 if ($value === 'fixed') {
-                    $value = CouponType::FIXED();
+                    $value = CouponType::Fixed;
                 }
 
                 if ($value === 'percentage') {
-                    $value = CouponType::PERCENTAGE();
+                    $value = CouponType::Percentage;
                 }
 
                 // If the value is already set, it's over 100 and it's a percentage, we want to divide it by 100...
-                if ($value === CouponType::PERCENTAGE() && $this->value > 100) {
+                if ($value === CouponType::Percentage && $this->value > 100) {
                     $this->value = $this->value / 100;
                 }
 
@@ -75,7 +74,7 @@ class Coupon implements Contract
                     $value = (int) str_replace('.', '', $value);
                 }
 
-                if ($this->type === CouponType::PERCENTAGE() && $value > 100) {
+                if ($this->type === CouponType::Percentage && $value > 100) {
                     return $value / 100;
                 }
 

resources/views/cp/coupons/index.blade.php
@@ -36,7 +36,7 @@
                             {{ $coupon->get('description') ?? '-' }}
                         </td>
                         <td>
-                            @if($coupon->type() === \DoubleThreeDigital\SimpleCommerce\Coupons\CouponType::PERCENTAGE())
+                            @if($coupon->type() === \DoubleThreeDigital\SimpleCommerce\Coupons\CouponType::Percentage)
                                 {{ $coupon->value() }}% off
                             @else
                                 {{ \DoubleThreeDigital\SimpleCommerce\Currency::parse($coupon->value(), \Statamic\Facades\Site::current()) }} off

src/Coupons/CouponType.php
@@ -2,12 +2,8 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Coupons;
 
-use Spatie\Enum\Enum;
-
-/**
- * @method static self FIXED()
- * @method static self PERCENTAGE()
- */
-class CouponType extends Enum
+enum CouponType: string
 {
+    case Fixed = 'fixed';
+    case Percentage = 'percentage';
 }

src/Exceptions/CheckoutProductHasNoStockException.php
@@ -15,11 +15,11 @@ class CheckoutProductHasNoStockException extends \Exception
         $this->product = $product;
         $this->variant = $variant;
 
-        if ($product->purchasableType() === ProductType::PRODUCT()) {
+        if ($product->purchasableType() === ProductType::Product) {
             $message = "Product [{$product->id()}] does not have any available stock.";
         }
 
-        if ($product->purchasableType() === ProductType::VARIANT()) {
+        if ($product->purchasableType() === ProductType::Variant) {
             $message = "Variant [{$variant->key()}] on [{$product->id()}] does not have any available stock.";
         }
 

src/Fieldtypes/ProductVariantFieldtype.php
@@ -44,7 +44,7 @@ class ProductVariantFieldtype extends Fieldtype
 
         $product = Product::find($value['product']);
 
-        if ($product->purchasableType() === ProductType::PRODUCT()) {
+        if ($product->purchasableType() === ProductType::Product) {
             return null;
         }
 

src/Http/Controllers/CartItemController.php
@@ -106,11 +106,11 @@ class CartItemController extends BaseActionController
         }
 
         // Ensure there's enough stock to fulfill the customer's quantity
-        if ($product->purchasableType() === ProductType::PRODUCT()) {
+        if ($product->purchasableType() === ProductType::Product) {
             if ($product->stock() && $product->stock() !== null && $product->stock() < $request->quantity) {
                 return $this->withErrors($request, __("There's not enough stock to fulfil the quantity you selected. Please try again later."));
             }
-        } elseif ($product->purchasableType() === ProductType::VARIANT()) {
+        } elseif ($product->purchasableType() === ProductType::Variant) {
             $variant = $product->variant($request->get('variant'));
 
             if ($variant !== null && $variant->stock() !== null && $variant->stock() < $request->quantity) {

src/Orders/Calculator.php
@@ -79,7 +79,7 @@ class Calculator implements Contract
     {
         $product = ProductAPI::find($lineItem['product']);
 
-        if ($product->purchasableType() === ProductType::VARIANT()) {
+        if ($product->purchasableType() === ProductType::Variant) {
             $variant = $product->variant(
                 isset($lineItem['variant']['variant']) ? $lineItem['variant']['variant'] : $lineItem['variant']
             );
@@ -173,11 +173,11 @@ class Calculator implements Contract
             $baseAmount = $data['items_total'] + $data['tax_total'];
 
             // Otherwise do all the other stuff...
-            if ($coupon->type() === CouponType::PERCENTAGE()) {
+            if ($coupon->type() === CouponType::Percentage) {
                 $data['coupon_total'] = (int) ($value * $baseAmount) / 100;
             }
 
-            if ($coupon->type() === CouponType::FIXED()) {
+            if ($coupon->type() === CouponType::Fixed) {
                 $data['coupon_total'] = (int) $baseAmount - ($baseAmount - $value);
             }
 

src/Orders/Checkout/HandleStock.php
@@ -18,7 +18,7 @@ class HandleStock
             ->each(function (LineItem $item) {
                 $product = $item->product();
 
-                if ($product->purchasableType() === ProductType::PRODUCT()) {
+                if ($product->purchasableType() === ProductType::Product) {
                     if (is_int($product->stock())) {
                         $stock = $product->stock() - $item->quantity();
 
@@ -41,7 +41,7 @@ class HandleStock
                     }
                 }
 
-                if ($product->purchasableType() === ProductType::VARIANT()) {
+                if ($product->purchasableType() === ProductType::Variant) {
                     $variant = $product->variant($item->variant()['variant'] ?? $item->variant());
 
                     if ($variant !== null && $variant->stock() !== null) {

commit 67de9080897b52f6666f82bcbe71d1a783bd84e6

Fix coupon types not being saved correctly
Caused by #712

src/Coupons/Coupon.php
@@ -206,11 +206,11 @@ class Coupon implements Contract
     public function toArray(): array
     {
         return array_merge($this->data()->toArray(), [
-            'id' => $this->id(),
-            'code' => $this->code(),
-            'value' => $this->value(),
-            'type' => $this->type(),
-            'enabled' => $this->enabled(),
+            'id' => $this->id,
+            'code' => $this->code,
+            'value' => $this->value,
+            'type' => optional($this->type)->value,
+            'enabled' => $this->enabled,
         ]);
     }
 