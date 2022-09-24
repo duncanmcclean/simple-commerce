a935f3efcf3b8c486a2f1a4a2571acfb2f820270
Duncan McClean <duncan@mcclean.co.uk>
2022-09-24 18:13:54 +0100

    Fix issue when updating stock on localised product when stock isn't localised

--- src/Orders/Checkout/HandleStock.php
+++ src/Orders/Checkout/HandleStock.php
@@ -7,8 +7,12 @@ use DoubleThreeDigital\SimpleCommerce\Contracts\Order;
 use DoubleThreeDigital\SimpleCommerce\Events\StockRunningLow;
 use DoubleThreeDigital\SimpleCommerce\Events\StockRunOut;
 use DoubleThreeDigital\SimpleCommerce\Exceptions\CheckoutProductHasNoStockException;
+use DoubleThreeDigital\SimpleCommerce\Facades\Product;
 use DoubleThreeDigital\SimpleCommerce\Orders\LineItem;
+use DoubleThreeDigital\SimpleCommerce\Products\EntryProductRepository;
 use DoubleThreeDigital\SimpleCommerce\Products\ProductType;
+use DoubleThreeDigital\SimpleCommerce\SimpleCommerce;
+use Statamic\Facades\Site;
 
 class HandleStock
 {
@@ -17,6 +21,16 @@ class HandleStock
         $order->lineItems()
             ->each(function (LineItem $item) {
                 $product = $item->product();
+
+                // Multi-site: Is the Stock field not localised? If so, we want the origin
+                // version of the product for stock purposes.
+                if (
+                    $this->isOrExtendsClass(SimpleCommerce::productDriver()['repository'], EntryProductRepository::class)
+                    && $product->resource()->hasOrigin()
+                    && ! $product->resource()->blueprint()->field('stock')->isLocalizable()
+                ) {
+                    $product = Product::find($product->resource()->origin()->id());
+                }
 
                 if ($product->purchasableType() === ProductType::Product) {
                     if (is_int($product->stock())) {
@@ -68,5 +82,11 @@ class HandleStock
             });
 
         return $next($order);
+    }
+
+    protected function isOrExtendsClass(string $class, string $classToCheckAgainst): bool
+    {
+        return is_subclass_of($class, $classToCheckAgainst)
+            || $class === $classToCheckAgainst;
     }
 }
