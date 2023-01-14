82e72f46c24210eb0b297ca1b08b7430b717f5f4
Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
2023-01-14 21:45:59 +0000

    Fix issue where orders field was being added to every blueprint (#786)
    * Fix issue where orders field was being added to every blueprint
    
    * Remove old imports

---
+++ src/Listeners/EnforceUserBlueprintFields.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\Listeners;
+
+use DoubleThreeDigital\SimpleCommerce\Customers\UserCustomerRepository;
+use DoubleThreeDigital\SimpleCommerce\Orders\EloquentOrderRepository;
+use DoubleThreeDigital\SimpleCommerce\Orders\EntryOrderRepository;
+use DoubleThreeDigital\SimpleCommerce\SimpleCommerce;
+use Statamic\Events\UserBlueprintFound;
+use Statamic\Fields\Blueprint;
+
+class EnforceUserBlueprintFields
+{
+    public function handle(UserBlueprintFound $event)
+    {
+        $customerDriver = SimpleCommerce::customerDriver();
+
+        if ($this->isOrExtendsClass($customerDriver['repository'], UserCustomerRepository::class)) {
+            return $this->enforceCustomerFields($event);
+        }
+    }
+
+    protected function enforceCustomerFields($event): Blueprint
+    {
+        $orderDriver = SimpleCommerce::orderDriver();
+
+        if ($this->isOrExtendsClass($orderDriver['repository'], EntryOrderRepository::class)) {
+            if (! $event->blueprint->hasField('orders')) {
+                $event->blueprint->ensureField('orders', [
+                    'type' => 'entries',
+                    'display' => __('Orders'),
+                    'mode' => 'default',
+                    'collections' => [
+                        $orderDriver['collection'],
+                    ],
+                    'create' => false,
+                ]);
+            }
+        }
+
+        if ($this->isOrExtendsClass($orderDriver['repository'], EloquentOrderRepository::class)) {
+            if (! $event->blueprint->hasField('orders')) {
+                $event->blueprint->ensureField('orders', [
+                    'type' => 'has_many',
+                    'display' => __('Orders'),
+                    'mode' => 'default',
+                    'resource' => 'orders',
+                    'create' => false,
+                ]);
+            }
+        }
+
+        return $event->blueprint;
+    }
+
+    protected function isOrExtendsClass(string $class, string $classToCheckAgainst): bool
+    {
+        return is_subclass_of($class, $classToCheckAgainst)
+            || $class === $classToCheckAgainst;
+    }
+}
