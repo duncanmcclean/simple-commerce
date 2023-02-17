2a27f8f868595e80c9eaf3fc9d1d4a56b6cd6e3e
Duncan McClean <duncan@duncanmcclean.com>
2023-02-17 16:46:32 +0000

    Ensure class name is being saved, not the object

--- src/Gateways/BaseGateway.php
+++ src/Gateways/BaseGateway.php
@@ -130,7 +130,7 @@ class BaseGateway
         // We need to ensure that the gateway is available in the
         // order when the OrderPaid event is dispatched.
         $order->gateway([
-            'use' => $this,
+            'use' => get_class($this),
             'data' => [],
         ]);
 
