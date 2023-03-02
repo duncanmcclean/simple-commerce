9c5879ba46bcf4576e1a1b73987af53f09918e39
Duncan McClean <duncan@duncanmcclean.com>
2023-03-02 18:33:14 +0000

    wip

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -110,6 +110,7 @@ class ServiceProvider extends AddonServiceProvider
         UpdateScripts\v5_0\UpdateNotificationsConfig::class,
         UpdateScripts\v5_0\UpdateOrderBlueprint::class,
     ];
+
     protected $vite = [
         'publicDirectory' => 'dist',
         'input' => [
