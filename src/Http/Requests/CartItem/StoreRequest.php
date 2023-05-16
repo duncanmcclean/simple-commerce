b510748997e7171c621ea34cb787e5207c6c7a2e
Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
2023-05-16 20:23:29 +0100

    Throw validation error when variant is missing (#870)
    * Add test to replicate issue
    
    * Ensure `variant` field is required when product is a variant product
    
    * Apply Code Style changes
    
    ---------
    
    Co-authored-by: duncanmcclean <duncanmcclean@users.noreply.github.com>

--- src/Http/Requests/CartItem/StoreRequest.php
+++ src/Http/Requests/CartItem/StoreRequest.php
@@ -2,11 +2,14 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Http\Requests\CartItem;
 
+use DoubleThreeDigital\SimpleCommerce\Facades\Product;
 use DoubleThreeDigital\SimpleCommerce\Http\Requests\AcceptsFormRequests;
 use DoubleThreeDigital\SimpleCommerce\Orders\Order as EntryOrder;
+use DoubleThreeDigital\SimpleCommerce\Products\ProductType;
 use DoubleThreeDigital\SimpleCommerce\Rules\ProductExists;
 use DoubleThreeDigital\SimpleCommerce\SimpleCommerce;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreRequest extends FormRequest
 {
@@ -20,20 +23,39 @@ class StoreRequest extends FormRequest
     public function rules()
     {
         $rules = [
-            'product' => ['required', 'string'],
-            'variant' => ['nullable', 'string'],
-            'quantity' => ['required', 'numeric', 'gt:0'],
-
-            'email' => ['nullable', 'email', function ($attribute, $value, $fail) {
-                if (preg_match('/^\S*$/u', $value) === 0) {
-                    return $fail(__('Your email may not contain any spaces.'));
-                }
-            }],
-            'customer.email' => ['nullable', 'email', function ($attribute, $value, $fail) {
-                if (preg_match('/^\S*$/u', $value) === 0) {
-                    return $fail(__('Your email may not contain any spaces.'));
-                }
-            }],
+            'product' => [
+                'required',
+                'string',
+            ],
+            'variant' => [
+                Rule::requiredIf(function () {
+                    return Product::find($this->product)->purchasableType() === ProductType::Variant;
+                }),
+                'string',
+            ],
+            'quantity' => [
+                'required',
+                'numeric',
+                'gt:0',
+            ],
+            'email' => [
+                'nullable',
+                'email',
+                function ($attribute, $value, $fail) {
+                    if (preg_match('/^\S*$/u', $value) === 0) {
+                        return $fail(__('Your email may not contain any spaces.'));
+                    }
+                },
+            ],
+            'customer.email' => [
+                'nullable',
+                'email',
+                function ($attribute, $value, $fail) {
+                    if (preg_match('/^\S*$/u', $value) === 0) {
+                        return $fail(__('Your email may not contain any spaces.'));
+                    }
+                },
+            ],
         ];
 
         if ($formRequest = $this->get('_request')) {
