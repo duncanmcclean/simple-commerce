e5016f06f1c7c4bd852b60db18b5cfe4a2dc23e7
Duncan McClean <duncan@duncanmcclean.com>
2024-01-29 23:02:29 +0000

    Refactor update class references query

--- src/Console/Commands/UpdateClassReferences.php
+++ src/Console/Commands/UpdateClassReferences.php
@@ -2,6 +2,7 @@
 
 namespace DoubleThreeDigital\SimpleCommerce\Console\Commands;
 
+use DoubleThreeDigital\SimpleCommerce\Facades\Order;
 use DoubleThreeDigital\SimpleCommerce\Orders\EloquentOrderRepository;
 use DoubleThreeDigital\SimpleCommerce\Orders\EntryOrderRepository;
 use DoubleThreeDigital\SimpleCommerce\Orders\OrderModel;
@@ -22,51 +23,25 @@ class UpdateClassReferences extends Command
     {
         $this->info('Updating class references...');
 
-        // TODO: refactor query
-        if ($this->isOrExtendsClass(SimpleCommerce::orderDriver()['repository'], EntryOrderRepository::class)) {
-            Collection::find(SimpleCommerce::orderDriver()['collection'])
-                ->queryEntries()
-                ->where('gateway', '!=', null)
-                ->chunk(50, function ($orders) {
-                    $orders->each(function ($entry) {
-                        // When the gateway reference is still a class, change it to the handle.
-                        if (class_exists($entry->get('gateway')['use'])) {
-                            $entry->set('gateway', array_merge($entry->get('gateway'), [
-                                'use' => $entry->get('gateway')['use']::handle(),
-                            ]))->saveQuietly();
-                        }
+        Order::query()
+            ->where('gateway', '!=', null)
+            ->chunk(50, function ($orders) {
+                $orders->each(function ($order) {
+                    // When the gateway reference is still a class, change it to the handle.
+                    if ($order->gateway() && class_exists($order->gateway()['use'])) {
+                        $order->gateway(array_merge($order->gateway(), [
+                            'use' => $order->gateway()['use']::handle(),
+                        ]));
 
-                        // When the shipping method reference is still a class, change it to the handle.
-                        if ($entry->has('shipping_method') && class_exists($entry->get('shipping_method'))) {
-                            $entry->set('shipping_method', $entry->get('shipping_method')::handle())->saveQuietly();
-                        }
-                    });
+                        $order->save();
+                    }
+
+                    // When the shipping method reference is still a class, change it to the handle.
+                    if ($order->has('shipping_method') && class_exists($order->get('shipping_method'))) {
+                        $order->set('shipping_method', $order->get('shipping_method')::handle())->saveQuietly();
+                    }
                 });
-        }
-
-        if ($this->isOrExtendsClass(SimpleCommerce::orderDriver()['repository'], EloquentOrderRepository::class)) {
-            OrderModel::query()
-                ->where('gateway', '!=', null)
-                ->chunk(50, function ($orders) {
-                    $orders->each(function ($order) {
-                        // When the gateway reference is still a class, change it to the handle.
-                        if (class_exists($order->gateway['use'])) {
-                            $order->gateway = array_merge($order->gateway, [
-                                'use' => $order->gateway['use']::handle(),
-                            ]);
-
-                            $order->saveQuietly();
-                        }
-
-                        // When the shipping method reference is still a class, change it to the handle.
-                        if (isset($order->data['shipping_method']) && class_exists($order->data['shipping_method'])) {
-                            $order->data['shipping_method'] = $order->data['shipping_method']::handle();
-
-                            $order->saveQuietly();
-                        }
-                    });
-                });
-        }
+            });
     }
 
     protected function isOrExtendsClass(string $class, string $classToCheckAgainst): bool
