24ad688885221efbdc180621d83e3772c956373d
Duncan McClean <duncan@duncanmcclean.com>
2024-02-03 16:27:43 +0000

    Re-do the UpdateClassReferences update script

---
+++ tests/UpdateScripts/UpdateClassReferencesTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Statamic\Facades\Entry;
+use DoubleThreeDigital\SimpleCommerce\UpdateScripts\v6_0\UpdateClassReferences;
+
+it('updates reference to gateway class', function () {
+    $orderEntry = Entry::make()
+        ->collection('orders')
+        ->id('test')
+        ->data(['gateway' => ['use' => 'DoubleThreeDigital\SimpleCommerce\Gateways\Builtin\StripeGateway', 'data' => ['id' => 'pi_1234']]]);
+
+    $orderEntry->save();
+
+    (new UpdateClassReferences('doublethreedigital/simple-commerce', '6.0.0'))->update();
+
+    $orderEntry->fresh();
+
+    expect($orderEntry->get('gateway'))->toBe([
+        'use' => 'stripe',
+        'data' => ['id' => 'pi_1234'],
+        'refund' => null,
+    ]);
+});
+
+it('updates reference to shipping method class', function () {
+    $orderEntry = Entry::make()
+        ->collection('orders')
+        ->id('test')
+        ->data(['shipping_method' => 'DoubleThreeDigital\SimpleCommerce\Shipping\FreeShipping']);
+
+    $orderEntry->save();
+
+    (new UpdateClassReferences('doublethreedigital/simple-commerce', '6.0.0'))->update();
+
+    $orderEntry->fresh();
+
+    expect($orderEntry->get('shipping_method'))->toBe('free_shipping');
+});
