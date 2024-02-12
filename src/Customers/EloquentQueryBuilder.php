6c8841d0c45494eb3899b7b81b880f0bf5e9d78b
Duncan McClean <duncan@duncanmcclean.com>
2024-02-12 12:11:07 +0000

    Refactor into its own method

--- src/Customers/EloquentQueryBuilder.php
+++ src/Customers/EloquentQueryBuilder.php
@@ -3,6 +3,8 @@
 namespace DuncanMcClean\SimpleCommerce\Customers;
 
 use DuncanMcClean\SimpleCommerce\Facades\Customer;
+use Illuminate\Support\Facades\Schema;
+use Statamic\Facades\Blink;
 use Statamic\Query\EloquentQueryBuilder as QueryEloquentQueryBuilder;
 
 class EloquentQueryBuilder extends QueryEloquentQueryBuilder
@@ -18,6 +20,23 @@ class EloquentQueryBuilder extends QueryEloquentQueryBuilder
             return $this->builder->getModel()->getKeyName();
         }
 
+        if (! $this->columnExists($column)) {
+            $column = "data->{$column}";
+        }
+
         return $column;
+    }
+
+    protected function columnExists(string $column): bool
+    {
+        $databaseColumns = Blink::once("DatabaseColumns_{$this->builder->getModel()->getTable()}", function () {
+            $columns = Schema::getConnection()
+                ->getDoctrineSchemaManager()
+                ->listTableColumns($this->builder->getModel()->getTable());
+
+            return collect($columns)->map->getName()->values();
+        });
+
+        return $databaseColumns->contains($column);
     }
 }
