1dcbe7331f38b851b734b74c091e9842951266ea
Duncan McClean <duncan@duncanmcclean.com>
2024-02-13 23:08:45 +0000

    [6.x] Prevent passing the cart to the view (#1013)
    * Prevent passing the cart to the view
    
    * Move to under the is_checkout_request stuff

--- src/Http/Controllers/BaseActionController.php
+++ src/Http/Controllers/BaseActionController.php
@@ -3,6 +3,7 @@
 namespace DuncanMcClean\SimpleCommerce\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Statamic\Http\Controllers\Controller;
 
 class BaseActionController extends Controller
@@ -18,12 +19,18 @@ class BaseActionController extends Controller
             return response()->json($data);
         }
 
-        if (isset($data['is_checkout_request'])) {
+        if (Arr::has($data, 'is_checkout_request')) {
             $request->session()->put('simple-commerce.checkout.success', [
                 'order_id' => $data['cart']['id'],
                 'expiry' => now()->addMinutes(30),
                 'url' => $request->_redirect,
             ]);
+        }
+
+        // The cart is only useful in a JSON response, so we'll remove it from
+        // the $data array before we pass it to the view.
+        if (Arr::has($data, 'cart')) {
+            unset($data['cart']);
         }
 
         return $request->_redirect ?
