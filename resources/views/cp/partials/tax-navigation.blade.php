2069182a4774841fd7ff1a1c9d0192f4842ea620
Duncan McClean <duncan@duncanmcclean.com>
2024-05-21 22:43:08 +0100

    Fix border on tax navigation

--- resources/views/cp/partials/tax-navigation.blade.php
+++ resources/views/cp/partials/tax-navigation.blade.php
@@ -1,4 +1,4 @@
-<div class="border-b px-2 text-sm flex">
+<div class="border-b dark:border-dark-300 px-2 text-sm flex">
     <a class="block data-list-filter-link @if(str_contains(Route::currentRouteName(), 'simple-commerce.tax-rates.index')) active @endif" href="{{ cp_route('simple-commerce.tax-rates.index') }}">
         {{ __('Tax Rates') }}
     </a>
