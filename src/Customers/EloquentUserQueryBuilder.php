ada34927d33e39fdf6c921e6bd1ef5d235a5b398
Duncan McClean <duncan@duncanmcclean.com>
2024-05-31 20:10:20 +0100

    Fix error when querying customers stored as database users (#1088)

--- src/Customers/EloquentUserQueryBuilder.php
+++ src/Customers/EloquentUserQueryBuilder.php
@@ -4,11 +4,14 @@ namespace DuncanMcClean\SimpleCommerce\Customers;
 
 use DuncanMcClean\SimpleCommerce\Facades\Customer;
 use Statamic\Auth\Eloquent\UserQueryBuilder;
+use Statamic\Facades\User;
 
 class EloquentUserQueryBuilder extends UserQueryBuilder
 {
     protected function transform($items, $columns = ['*'])
     {
-        return $items->map(fn ($item) => Customer::fromModel($item));
+        return $items
+            ->map(fn ($item) => User::make()->model($item))
+            ->map(fn ($item) => Customer::fromUser($item));
     }
 }
