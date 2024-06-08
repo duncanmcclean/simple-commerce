6c40043023e331c94c47e92182fc715bf1dd547d
Web10 <129582223+Web10-Joris@users.noreply.github.com>
2024-06-08 20:26:31 +0200

    [7.x] Add config option to set cookie expiry for carts (#1089)
    * add: Option to set Cookie expiry for carts
    
    * simplify
    
    ---------
    
    Co-authored-by: Duncan McClean <duncan@duncanmcclean.com>

--- src/Orders/Cart/Drivers/CookieDriver.php
+++ src/Orders/Cart/Drivers/CookieDriver.php
@@ -52,7 +52,11 @@ class CookieDriver implements CartDriver
         $cart->set('site', $this->guessSiteFromRequest()->handle());
         $cart->save();
 
-        Cookie::queue($this->getKey(), $cart->id);
+        Cookie::queue(
+            $this->getKey(),
+            $cart->id,
+            Config::get('simple-commerce.cart.expiration', 0)
+        );
 
         // Because the cookie won't be set until the end of the request,
         // we need to set it somewhere for the remainder of the request.
