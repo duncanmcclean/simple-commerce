d64a19865ca001e9629fe2726ef035e1a7f11094
Duncan McClean <duncan@duncanmcclean.com>
2024-08-03 21:41:21 +0100

    wip

--- tests/Data/OrderTest.php
+++ tests/Data/OrderTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Data;
 
 use DuncanMcClean\SimpleCommerce\Facades\Order;
+use DuncanMcClean\SimpleCommerce\Orders\LineItem;
 use DuncanMcClean\SimpleCommerce\Orders\OrderStatus;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
@@ -24,18 +25,103 @@ class OrderTest extends TestCase
     #[Test]
     public function can_add_line_item()
     {
-        $this->markTestIncomplete();
+        $this->markTestIncomplete('Need to implement the product repositories before this test can be completed.');
+
+        $order = Order::make();
+
+        $order->lineItems()->create([
+            'product' => 'product-id',
+            'variant' => 'variant-id',
+            'quantity' => 2,
+            'total' => 1000,
+            'foo' => 'bar',
+            'baz' => 'qux',
+        ]);
+
+        $this->assertCount(1, $order->lineItems());
+
+        $lineItem = $order->lineItems()->first();
+
+        $this->assertInstanceOf(LineItem::class, $lineItem);
+        $this->assertNotNull($lineItem->id());
+//        $this->assertEquals('product-id', $lineItem->product()->id());
+//        $this->assertEquals('variant-id', $lineItem->variant()->id());
+        $this->assertEquals(2, $lineItem->quantity());
+        $this->assertEquals(1000, $lineItem->total());
+        $this->assertEquals('bar', $lineItem->data()->get('foo'));
+        $this->assertEquals('qux', $lineItem->data()->get('baz'));
     }
 
     #[Test]
     public function can_update_line_item()
     {
-        $this->markTestIncomplete();
+        $this->markTestIncomplete('Need to implement the product repositories before this test can be completed.');
+
+        $order = Order::make()->lineItems([
+            [
+                'id' => 'abc123',
+                'product' => 'product-id',
+                'variant' => 'variant-id',
+                'quantity' => 2,
+                'total' => 1000,
+                'foo' => 'bar',
+                'baz' => 'qux',
+            ],
+            [
+                'id' => 'def456',
+                'product' => 'another-product-id',
+                'quantity' => 1,
+                'total' => 2500,
+                'bar' => 'baz',
+            ]
+        ]);
+
+        $order->lineItems()->update('abc123', [
+            'product' => 'product-id',
+            'variant' => 'variant-id',
+            'quantity' => 1, // This changed...
+            'total' => 500, // This changed too...
+            'barz' => 'foo', // This is new...
+            // And, some other keys were removed...
+        ]);
+
+        $lineItem = $order->lineItems()->find('abc123');
+
+        $this->assertEquals(1, $lineItem->quantity());
+        $this->assertEquals(500, $lineItem->total());
+        $this->assertNull($lineItem->data()->get('foo'));
+        $this->assertNull($lineItem->data()->get('baz'));
+        $this->assertEquals('foo', $lineItem->data()->get('barz'));
     }
 
     #[Test]
     public function can_remove_line_item()
     {
-        $this->markTestIncomplete();
+        $order = Order::make()->lineItems([
+            [
+                'id' => 'abc123',
+                'product' => 'product-id',
+                'variant' => 'variant-id',
+                'quantity' => 2,
+                'total' => 1000,
+                'foo' => 'bar',
+                'baz' => 'qux',
+            ],
+            [
+                'id' => 'def456',
+                'product' => 'another-product-id',
+                'quantity' => 1,
+                'total' => 2500,
+                'bar' => 'baz',
+            ]
+        ]);
+
+        $this->assertCount(2, $order->lineItems());
+
+        $order->lineItems()->remove('abc123');
+
+        $this->assertCount(1, $order->lineItems());
+        $this->assertNull($order->lineItems()->find('abc123'));
+        $this->assertNotNull($order->lineItems()->find('def456'));
     }
 }
