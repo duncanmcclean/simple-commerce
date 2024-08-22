8fe980bf7ddf3d932d15f04cf2087093e407e482
Duncan McClean <duncan@duncanmcclean.com>
2024-08-22 23:00:26 +0100

    wip

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,31 +1,26 @@
-// Fieldtypes
-import OrderReceiptFieldtype from './components/Fieldtypes/OrderReceiptFieldtype.vue'
-import MoneyFieldtype from './components/Fieldtypes/MoneyFieldtype.vue'
-import ProductVariantsFieldtype from './components/Fieldtypes/ProductVariants/ProductVariantsFieldtype.vue'
-import CustomerFieldtype from './components/Fieldtypes/CustomerFieldtype.vue'
-import CustomerFieldtypeIndex from './components/Fieldtypes/CustomerFieldtypeIndex.vue'
-
-Statamic.$components.register('order_receipt-fieldtype', OrderReceiptFieldtype)
-Statamic.$components.register('money-fieldtype', MoneyFieldtype)
-Statamic.$components.register(
-    'product-variants-fieldtype',
-    ProductVariantsFieldtype
-)
-Statamic.$components.register('customer-fieldtype', CustomerFieldtype)
-Statamic.$components.register('customer-fieldtype-index', CustomerFieldtypeIndex)
-
-// Listings
-import CouponListing from './components/Listings/CouponListing.vue'
-Statamic.$components.register('coupon-listing', CouponListing)
-
-// Widgets
-import OrdersChart from './components/Widgets/OrdersChart.vue'
-Statamic.$components.register('orders-chart', OrdersChart)
-
-
-// Fresh... todo: tidy up this file
+import CouponListing from './components/coupons/Listing.vue'
+import CustomerFieldtype from './components/fieldtypes/CustomerFieldtype.vue'
+import CustomerFieldtypeIndex from './components/fieldtypes/CustomerFieldtypeIndex.vue'
+import MoneyFieldtype from './components/fieldtypes/MoneyFieldtype.vue'
+import OrderReceiptFieldtype from './components/fieldtypes/OrderReceiptFieldtype.vue'
+import ProductVariantsFieldtype from './components/fieldtypes/ProductVariants/ProductVariantsFieldtype.vue'
 import OrdersListing from './components/orders/Listing.vue'
 import OrdersPublishForm from './components/orders/PublishForm.vue'
+import OrdersChart from './components/widgets/OrdersChart.vue'
 
+// Coupons
+// Statamic.$components.register('coupon-listing', CouponListing)
+
+// Fieldtypes
+Statamic.$components.register('customer-fieldtype', CustomerFieldtype)
+Statamic.$components.register('customer-fieldtype-index', CustomerFieldtypeIndex)
+Statamic.$components.register('money-fieldtype', MoneyFieldtype)
+Statamic.$components.register('order_receipt-fieldtype', OrderReceiptFieldtype)
+Statamic.$components.register('product-variants-fieldtype', ProductVariantsFieldtype)
+
+// Orders
 Statamic.$components.register('orders-listing', OrdersListing)
 Statamic.$components.register('orders-publish-form', OrdersPublishForm)
+
+// Widgets
+// Statamic.$components.register('orders-chart', OrdersChart)
