04efb8a89e0b9a6059489e06a68ec79d4c07be01
Duncan McClean <duncan@duncanmcclean.com>
2024-11-11 23:06:53 +0000

    wip

--- src/Http/Controllers/CP/Orders/DownloadPackingSlipController.php
+++ src/Http/Controllers/CP/Orders/DownloadPackingSlipController.php
@@ -2,36 +2,21 @@
 
 namespace DuncanMcClean\SimpleCommerce\Http\Controllers\CP\Orders;
 
-use Dompdf\Dompdf;
-use Dompdf\Options;
 use DuncanMcClean\SimpleCommerce\Facades\Order;
 use Illuminate\Http\Request;
 use Statamic\Http\Controllers\CP\CpController;
-use Facades\Statamic\Console\Processes\Composer;
 
 class DownloadPackingSlipController extends CpController
 {
     public function __invoke(Request $request, $order)
     {
-        if (! Composer::isInstalled('dompdf/dompdf')) {
-            throw new \Exception("Please require `dompdf/dompdf` to print packing slips.");
-        }
-
         $order = Order::find($order);
 
         $this->authorize('edit', $order);
 
-        $dompdfOptions = new Options;
-        $dompdfOptions->setChroot(base_path());
-
-        $pdf = (new Dompdf($dompdfOptions))->setPaper('letter');
-
-        $pdf->loadHtml(view('simple-commerce::packing-slip', [
+        return view('simple-commerce::packing-slip', [
             'config' => config()->all(),
             'order' => $order,
-        ])->render());
-
-        // todo: this doesn't seem to be returning any pages...
-        return response($pdf->render())->header('Content-Type', 'application/pdf');
+        ]);
     }
 }
