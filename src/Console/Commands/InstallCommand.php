6a42e18214c94e14e2db456f4765accf57fbf3db
Duncan McClean <duncan@duncanmcclean.com>
2025-02-09 00:24:19 +0000

    wip

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -2,11 +2,18 @@
 
 namespace DuncanMcClean\SimpleCommerce\Console\Commands;
 
+use DuncanMcClean\SimpleCommerce\Data\Currencies;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Statamic\Console\RunsInPlease;
+use Statamic\Facades\Collection;
+use Statamic\Facades\Site;
+use Statamic\Support\Str;
+use Stillat\Proteus\Support\Facades\ConfigWriter;
 
-use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\search;
+use function Laravel\Prompts\select;
+use function Laravel\Prompts\text;
 
 class InstallCommand extends Command
 {
@@ -18,21 +25,90 @@ class InstallCommand extends Command
 
     public function handle(): void
     {
+        // todo: configure database orders
+
         $this
-            ->promptToCreateProductsCollection()
+            ->publishConfig()
+            ->promptForSiteCurrencies()
+            ->promptForProductsCollection()
             ->promptToPublishCheckoutStubs();
+
+        $this->line('  <fg=green;options=bold>Simple Commerce has been installed!</> 🎉');
+        $this->newLine();
+        $this->line('  For more information on getting started, please review the documentation: <comment>https://simple-commerce.duncanmcclean.com</comment>');
     }
 
-    private function promptToCreateProductsCollection(): self
+    private function publishConfig(): self
     {
-        // todo
+        $this->call('vendor:publish', [
+            '--tag' => 'simple-commerce-config',
+            '--force' => true,
+        ]);
+
+        return $this;
+    }
+
+    private function promptForSiteCurrencies(): self
+    {
+        $sites = Site::all()->map(function ($site) {
+            $currency = search(
+                label: "Which currency should the {$site->name()} site use?",
+                options: fn (string $value) => Currencies::mapWithKeys(fn ($currency) => [$currency['code'] => "{$currency['name']} ({$currency['code']})"])
+                    ->when($site->attribute('currency'), fn ($currencies) => $currencies->filter(fn ($name, $code) => $code === $site->attribute('currency')))
+                    ->when(strlen($value) > 0, fn ($currencies) => $currencies->filter(fn ($name) => Str::contains($name, $value, ignoreCase: true)))
+                    ->all(),
+            );
+
+            return array_merge($site->rawConfig(), [
+                'attributes' => array_merge($site->attributes(), ['currency' => $currency]),
+            ]);
+        });
+
+        Site::setSites($sites->all())->save();
+
+        return $this;
+    }
+
+    private function promptForProductsCollection(): self
+    {
+        $collection = select(
+            label: 'Which collection contains your products?',
+            options: Collection::all()
+                ->mapWithKeys(fn ($collection) => [$collection->handle() => $collection->title()])
+                ->push('Create a "Products" collection')
+                ->all(),
+            hint: 'If you need to, you can always add additional collections later.'
+        );
+
+        if ($collection === 'Create a "Products" collection') {
+            $name = text('What should the collection be called?', default: 'Products');
+
+            Collection::make($collection = Str::studly($name))
+                ->title($name)
+                ->routes([Site::default()->handle() => Str::plural(Str::kebab($name)).'/{slug}'])
+                ->save();
+        }
+
+        ConfigWriter::write('statamic.simple-commerce.products.collections', [$collection]);
 
         return $this;
     }
 
     private function promptToPublishCheckoutStubs(): self
     {
-        if (! confirm('Would you like to publish the checkout stubs?')) {
+        if (File::exists(resource_path('views/checkout'))) {
+            return $this;
+        }
+
+        $this->line('  Simple Commerce is designed to be flexible and customizable, and that includes the checkout process.');
+        $this->line('  You can either publish the pre-built Checkout page provided by Simple Commerce, or build your own from scratch.');
+
+        $choice = select('What would you like to do?', [
+            'Publish the pre-built Checkout page',
+            'I will build my own Checkout page',
+        ]);
+
+        if ($choice === 'I will build my own Checkout page') {
             return $this;
         }
 
@@ -43,7 +119,7 @@ class InstallCommand extends Command
 
         $routes = File::get(base_path('routes/web.php'));
 
-        $routes .= <<<PHP
+        $routes .= <<<'PHP'
 
 Route::statamic('checkout', 'checkout.index', ['title' => 'Checkout', 'layout' => 'checkout.layout'])
     ->name('checkout');
@@ -55,7 +131,7 @@ PHP;
 
         File::put(base_path('routes/web.php'), $routes);
 
-        $this->components->info("Published. You'll find the checkout stubs in resources/views/checkout.");
+        $this->components->info("Published. You'll find the Checkout views in <comment>resources/views/checkout</comment>. Feel free to customize them as necessary.");
 
         return $this;
     }
