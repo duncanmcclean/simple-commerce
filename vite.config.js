a4026a0f80311f025a08681d648761710546a7a6
Duncan McClean <duncan@duncanmcclean.com>
2025-08-21 10:50:46 +0100

    Uncomment .css input

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
             publicDirectory: 'dist',
             input: [
                 'resources/js/cp.js',
-                // 'resources/css/cp.css',
+                'resources/css/cp.css',
             ],
         }),
     ],
